3bd81a1a51c83de33544beb07381dbc008d0b4dd
stinemarie <stinemarie86@dhcp-158-39-192-254.wlan.hioa.no>
2013-11-02 12:40:45 +0100

    prøver å lage karusell

--- Stine/karusell.inc.php
+++ Stine/karusell.inc.php
@@ -1,34 +1,30 @@
-<nav>
+<nav class="karusellen">
 	<ul>
 		<li>
-			<a href="">
 				<img src="./felter/felt1.jpg" alt="Trening">
-			</a>
+				<a class="next" href="#">neste</a>
 		</li>
 		<li>
-			<a href="">
-				<img src="./felter/felt1.jpg" alt="Kosthold">
-			</a>
+				<img src="./felter/felt2.jpg" alt="Kosthold">
+				<a class="next" href="#">neste</a> <a class="previous" href="#">forrige</a>
 		</li>
 		<li>
-			<a href="">
-				<img src="./felter/felt1.jpg" alt="Personlig Trener">
-			</a>
+				<img src="./felter/felt3.jpg" alt="Personlig Trener">
+				<a class="next" href="#">neste</a><a class="previous" href="#">forrige</a>
 		</li>
 		<li>
-			<a href="">
-				<img src="./felter/felt1.jpg" alt="Lenker">
-			</a>
+				<img src="./felter/felt4.jpg" alt="Lenker">
+				<a class="next" href="#">neste</a><a class="previous" href="#">forrige</a>
 		</li>
 		<li>
-			<a href="">
-				<img src="./felter/felt1.jpg" alt="Om oss">
-			</a>
+				<img src="./felter/felt3.jpg" alt="Om oss">
+				<a class="previous" href="#">forrige</a><a class="startover" href="#">rundt</a>
 		</li>
 	</ul>	
 </nav>
 
 <script>
+
 $(window).load (function() {
 	var theImage = $('ul li img');
 	var theWidth = theImage.width()
@@ -54,5 +50,25 @@ $(window).load (function() {
 	});		
  
 });//doc ready
+
+$(theImage).each(		
+function(intIndex){				
+$(this).nextAll('a')
+	.bind("click", function(){
+		if($(this).is(".next"))	{
+			$(this).parent('li').parent('ul').animate({
+				"margin-left": (-(intIndex + 1) * theWidth)				
+					}, 1000)	
+			} else if($(this).is(".previous")){
+			$(this).parent('li').parent('ul').animate({
+				"margin-left": (-(intIndex - 1) * theWidth)				
+			}, 1000)	
+			} else if($(this).is(".startover")){
+			$(this).parent('li').parent('ul').animate({
+				"margin-left": (0)				
+			}, 1000)
+	}
+	});//close .bind()									 
+});//close .each()
  
 </script>
