f11bbefe15ccf28ccebcef400f2ee478de045427
amundring <amund@Amunds-MacBook-Pro.local>
2013-11-03 19:07:31 +0100

    JQuery for å vise undermenyer ved hovering
    Gjorde også knappene klickbare med CSS

---
+++ Amund/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {	
+
+	$("#trening").hover(function(){
+  		$("#treningsNav").toggle('fast');
+	});
+
+	$("#kosthold").hover(function(){
+  		$("#kostholdsNav").toggle('fast');
+	});
+
+	$("#pt").hover(function(){
+  		$("#ptNav").toggle('fast');
+	});
+
+	$("#lenker").hover(function(){
+  		$("#lenkeNav").toggle('fast');
+	});
+
+	$("#om").hover(function(){
+  		$("#omNav").toggle('fast');
+	});
+	
+});
