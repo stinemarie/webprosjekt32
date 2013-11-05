2d86b5b0a84952bf2b41ddf04237fb3fe4dd838b
stinemarie <stinemarie86@Stine-MA-Grumhedens-MacBook-Pro.local>
2013-11-05 21:03:39 +0100

    la til css for bildegalleri for oppskrifter

--- Stine/oppskrifter2.inc.php
+++ Stine/oppskrifter2.inc.php
@@ -1,6 +1,44 @@
+<style>
+	.oppskrift *{
+		padding: 0;
+		margin: 0;
+	}
+
+	.oppskrift .fargefelt {
+		display: block
+	}
+
+	.oppskrift ul li{
+		list-style: none;
+		float: left;
+	}
+
+	.oppskrift .overlay{
+		height: 220px;
+		width: 220px;
+		background-color: rgba(236, 237, 162, 0.7);
+		position: absolute;
+		top:0;
+		right:0;
+		display: none;
+	}
+
+	.oppskrift .tekst{
+	color: white;
+	/*color: #9eb728;*/
+	font-size: 30px;
+	font-family: "Gill Sans", sans-serif;
+	margin: 20px;
+	overflow: hidden;
+	text-shadow: 1px 1px #4C4C4C;
+}
+
+
+</style>
+
 <nav class="oppskrift">
 	<div>
-		<ul id="naliste">
+		<ul>
 			<li>
 				<a class="lenke" href="">
 					<img class="fargefelt" src="./bilderoppskrift/fisk1.jpg" alt="oppskrift på fisk">
