3b6689997e6d49522fd78c57df58818f8932a0f4
stinemarie <stinemarie86@dhcp-158-39-192-254.wlan.hioa.no>
2013-11-05 15:56:06 +0100

    laget ny utgave av oppskrift

---
+++ Stine/oppskrifter2.inc.php
@@ -0,0 +1,9 @@
+<nav class="oppskrifter">
+	<div>
+		<ul>
+			<li>
+				<a href="">
+					<img src"./bildeoppskrift/fiskt1.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på fisk</p>
+					
