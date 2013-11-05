81e7f8947ce592bea98b5e1d88b1e0a35e294fa6
stinemarie <stinemarie86@dhcp-158-39-192-254.wlan.hioa.no>
2013-11-05 11:16:43 +0100

    endre på trening sider

--- Stine/trening/treninggen.inc.php
+++ Stine/trening/treninggen.inc.php
@@ -1,18 +1,3 @@
-<h1>Hvorfor trene?</h1>
-<p>Det er utrolig viktig å sette av tid til trening hver uke. 
-Trening er noe som gir deg mer energi til det du skal få gjort i hverdagen, 
-samtidig så vil jevnlig trening gjøre at du styrker hjerte. Dette forebygger 
-hjerte-karsykdommer.Det er viktig å kombinere med både styrke og kondisjon 
-for å få litt variert trening. Husk også å få noen hviledager for å la kroppen 
-få restituert.</p>
-
-<!-- disse sidene skal vi ha:
-yoga og pilates
-kondisjonstrening
-langløping
-spinning
-styrketrening -->
-
 
 <nav class="treningssider">
 	<ul>
