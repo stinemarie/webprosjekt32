458c7af4829d147cd9cd880667bcea1a99605464
stinemarie <stinemarie86@Stine-MA-Grumhedens-MacBook-Pro.local>
2013-11-05 21:17:39 +0100

    la til hrml for oppskrifter og flyttet bildefiler inn i img filen

--- Websiden/pages/oppskrifter.php
+++ Websiden/pages/oppskrifter.php
@@ -1,1 +1,65 @@
 <h2>Oppskrifter</h2>
+
+
+<nav class="oppskrift">
+	<div>
+		<ul>
+			<li>
+				<a class="lenke" href="">
+					<img class="fargefelt" src="../img/fisk1.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på fisk</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="lenke" href="">
+					<img class="fargefelt" src="../img/salat1.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på salat</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="lenke" href="">
+					<img class="fargefelt" src="../img/salat2.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på salat</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="lenke" href="">
+					<img class="fargefelt" src="../img/smoothie1.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på smoothie</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="lenke" href="">
+					<img class="fargefelt" src="../img/smoothie2.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på smoothie</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="lenke" href="">
+					<img class="fargefelt" src="../img/terte1.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på brinebær terte med vaniljekrem</p>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="lenke" href="">
+					<img class="fargefelt" src="../img/suppe1.jpg" alt="oppskrift på fisk">
+					<div class="overlay">
+						<p class="tekst">trykk her for oppskrift på suppe</p>
+					</div>
+				</a>
+			</li>
+		</ul>
+	</div>
+</nav>
