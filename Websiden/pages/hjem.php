fc11bab25cc862e1902cbff7fb409a4054f944c4
stinemarie <stinemarie86@dhcp-158-39-192-254.wlan.hioa.no>
2013-11-06 17:37:06 +0100

    fikset karusell

--- Websiden/pages/hjem.php
+++ Websiden/pages/hjem.php
@@ -3,12 +3,12 @@
 <nav class="kar">
 	<div id="ramme">
 		<ul>
-			<li><div id="alpha">	<p>Les mer om Personlige Trenere</p><img id="alpha" src="img/fisk1.jpg" 	alt="Personlig Trener">	</div></li>
-			<li><div id="beta">		<p>Les mer om Kosthold 	</p>		<img id="beta" src="img/salat1.jpg" 	alt="Kosthold">			</div></li>
-			<li><div id="gamma">	<p>Les mer om Trening 	</p>		<img id="gamma" src="img/suppe1.jpg" 	alt="Trening">			</div></li>
-			<li><div id="delta">	<p>Lenker				</p>		<img d="delta" src="img/smoothie1.jpg"alt="Lenker">			</div></li>
+			<li><div id="alpha"><p>trening</p><a href=""><img src="./img/fisk1.jpg"></a></div></li>
+			<li><div id="beta"><p>kosthold</p><a href=""><img src="./img/suppe1.jpg"></a></div></li>
+			<li><div id="gamma"><p>helse</p><a href=""><img src="./img/suppe2.jpg"></a></div></li>
+			<li><div id="delta"><p>personlig trener</p><a href=""><img src="./img/smoothie1.jpg"></a></div></li>
 		</ul>
-		<div id="tilbake"></div> <div id="neste"></div>
+				<div id="tilbake"></div><div id="neste"></div>
 	</div><!--  slutt på ramme -->
 </nav>	
 <!-- <li><div id="beta"><p>les mer om kosthold</p><img src="img/salat1.jpg" alt="Kosthold"></div></li> -->
