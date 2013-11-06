3ceed5d1159eb38a548c97304591a5e11b6b1824
idaings <IdaVikaskagSalvesen@Ida-Vikaskag-Salvesens-MacBook-Pro.local>
2013-11-06 13:45:54 +0100

    Fylt inn tekst på Oppskrift.php
    Skrevet inn alle oppsrkiftene og lagt til bilde på hver oppskrift
    (derfor lagt til noen bilder i img mappen også)

--- Websiden/pages/oppskrifter.php
+++ Websiden/pages/oppskrifter.php
@@ -63,3 +63,193 @@
 		</ul>
 	</div>
 </nav>
+
+
+<h4>Pasta med rød pesto, kylling og paprika (3porsjoner)</h4>
+	<ul>
+		<li>2 Kyllingfilleter</li>
+		<li>Salt og pepper</li>
+		<li>Smør/olje til steking</li>
+		<li>1 Rødløk, i tynne skiver</li>
+		<li>1 Porsjon rød pesto</li>
+		<li>Parmesan, for litt luksus på toppen</li>
+		<li>3 Porsjoner fullkornspasta</li>
+		<li>Evt frisk persille eller andre krydderurter finhakket</li>
+	</ul>
+	<p>Kok pastaen som anvist på pakken. Tips til når den er ferdigkokt: Ha den i et dørslag og rør evt inn litt olje 
+		(gjerne chiliolje for ekstra smak) slik at man hindrer at den kleber seg sammen.</p>
+	<p>Krydre kyllingen med salt og pepper, og stek den raskt i smør/olje, til den er gjennomstekt. 
+		Legg kyllingen til side, og fres rødløk og paprika til rødløken er blank og myk.</p>	
+	<p>Bland sammen pasta, rød pesto, kulling og grønnsaker, og bland det hele forsiktig sammen. 
+		Strø gjerne over de friske krydderurtene hvis du har, og server med noen flak parmesan. For en siste finish, 
+		så kan du ha ferskt brød på siden!</p>
+	<img src="img/pastakylling.jpg" width="500px" alt="Pasta med rød pesto, kylling og paprika">
+<br>
+<h4>Brokkoliwok med spekeskinke, soltørkede tomater og pinjekjerner (2porsjoner)</h4>
+	<ul>
+		<li>1 Rødløk, i tynne båter</li>
+		<li>2 Fedd hvitløk, finkakket</li>
+		<li>1 Stor brokkoli, i små buketter (bruk gjerne stilken og, mye næring i den)</li>
+		<li>4-6 Skiver spekeskinke, i strimler (kan evt. bruke svinekjøtt også)</li>
+		<li>5-6 Soltørkede tomater, i biter (billig om man kjøper på glass i olje)</li>
+		<li>En neve pinjekjerner, ristet </li>
+		<li>Persille, hakket</li>
+		<li>Nykvernet pepper</li>
+	</ul>
+	<p>Fres løken i litt smør/olje til den er blank og myk. Ha i hvitløken og brokkolien, og fres til brokkolien er gjennomvarm 
+		og passe mør. Pass på at hvitløken ikke blir brent (tilsett evt. litt ekstra smør/olje og salt for å unngå dette). Rør i tomatene og spekeskinken.</p>
+	<p>Når tomatene er  gjennomvarme tilsettes de ristede pinjekjernene og den hakkede persillen. Smak det hele til med nykvernet 
+		pepper, og evt. salt. Server det hele med godt brød, evt foccacia. Om du ikke har kronene til spekeskinken, så kan dette også 
+		være en fin vegetarrett, ved å evt. bare legge til noen ekstra grønnsaker, eller bare ha den som den er!</p>
+	<img src="img/brokkoliwok.jpg" width="500px" alt="Brokkoliwok">
+<br>
+<h4>Laks med sesamfrø, soyasaus og lun pak choysalat (3porsjoner)</h4>
+	<p>Laks med samfrø:</p>
+	<ul>
+		<li>500-600g laksefilet, i porsjonsstykker (kjøp frossent og tin, billigst)</li>
+		<li>salt og pepper</li>
+		<li>olje/smør til steking</li>
+		<li>sesamfrø</li>
+	</ul>
+	<p>Lun pak choysalat:</p>
+	<ul>
+		<li>1 pak choy (kinasalat), grovt strimlet</li>
+		<li>1 rødløk, i tynne båter</li>
+		<li>1 rød paprika, i strimler</li>
+		<li>2 gulrøtter, i strimler</li>
+	</ul>
+	<p>Spicy soyasaus:</p>
+	<ul>
+		<li>1 fedd hvitløk, finhakket</li>
+		<li>ca ¼ rød chili (eller mer etter ønske), finhakket</li>	
+		<li>ca ½ lime, saft og skall</li>
+		<li>2-3 ss olivenolje</li>
+		<li>2-3 ss soyasaus</li>
+		<li>1-2 ss ketjap manis (søt soyasaus)</li>
+		<li>1 ts flytende honning</li>
+		<li>frisk koriander, finhakket</li>
+	</ul>
+	<p>Evt tilbehød: 3porsjoner ris eller nudler (billigste løsningen)</p>
+	<p>Rør sammen ingrediensene til soyasausen. Smak deg frem her til du finner den balansen som du ønsker mellom de gode smakene. 
+		Har du frisk ingefør kan dette også tilsettes litt av, revet.</p>
+	<p>Krydre lakesen med salt og stek den ca 2-4min på hver side i stekepannen, til den er passe stekt. Bruk fjerne først en 
+		nøytral olje og deretter en ss smør når laksen har fått stekeskorpe på begge sidene. Mot slutten av steketiden strør du 
+		litt sesamfrø på laksen. Laksen er ferdigstekt, når den så vidt faller fra hverandre. La laksen hvile i noen minutter før 
+		den serveres.</p>
+	<p>Skjær opp grønnsakene som angitt ovenfor og wok dem raskt i en stekepanne, i en nøytral olje. Grønnsakene er ferdige når 
+		de er nesten møre og fortsatt har litt ”spenst” igjen. Om man ikke får tak i pak choy, kan man evt heller benytte spinat.</p>
+	<p>Det hele serveres samlet, med kokt ris eller nudler som tilbehør. Man har da til slutt soyasausen over den ferdige retten, 
+		eller serverer den ved siden av slik at man kan forsyne seg etter smak og behag.</p>
+	<img src="img/laksesalat.jpg" width="500px" alt="Laks med sesamfrø">
+<br>
+<h4>Bønne- og linsesuppe med chorizo(3porsjoner+)</h4>
+	<ul>
+		<li>1 løk, finhakket</li>
+		<li>½ - ¾ rød chili (eller mer/mindre etter ønske), finhakket</li>
+		<li>1,5 terninger kyllingbujong/kyllingfond</li>
+		<li>3 ss tomatpure</li>
+		<li>1 boks hermetiske tomater, hakket</li>
+		<li>6-7 dl vann</li>
+		<li>1 boks hvite bønner</li>
+		<li>3 dl linser</li>
+		<li>5 soltørkede tomater, i biter</li>
+		<li>5 vårløk, i skiver</li>
+		<li>salt og nykvernet sort pepper</li>
+		<li>ca 1 ts revet ingefær</li>
+		<li>100 g chorizo, i terninger (dette kan man skippe om man vil)</li>
+		<li>Finhakket koriander til pynt</li>
+	</ul>
+	<p>Fres løk, hvitløk og chili i litt olje (og salt evt for at det ikke skal brenne), til løken er myk og blank. 
+		Tilsett tomatpuré, buljong/fond, hermetiske tomater, vann, bønner, linser og soltørkede tomater, og gi det hele et oppkok. 
+		Smak til med ingefær. La det stå og putre under lokk i ca 10min. I mellomtiden så kan du steke vårløken og chorizoen i litt smør og sette det til side.</p>
+	<p>Når suppen er kokt ferdig og har fått litt tykkelse, tilsetter man vårløk og chorizo, og smak deretter til suppen med 
+		salt og pepper. Strø over litt finhakket koriander og server evt med en liten klatt creme fraiche og ferskt brød til.</p>
+	<img src="img/chorizosuppe.jpg" width="500px" alt="Suppe">
+<br>
+<h4>Franks bringebærterte med vaniljekrem(4porsjoner)</h4>
+	<ul>
+		<li>2,5dl hvetemel</li>
+ 		<li>1 ss sukker</li>
+ 		<li>110 g smør</li>
+ 		<li>4-8 ss kaldt vann</li>
+ 		<li>1 kurv friske bringebær</li>
+ 		<li>ca 4 dl vaniljekrem (<a href="oppskrifter.html#vaniljekrem">Her kan man lage egen</a> eller kjøpe ferdig)</li>
+	 	<li>melisdryss</li>
+	 </ul>
+ 	<p>Ha hvetemel og sukker i en kjøkkenmaskin og tilsett smøret (i terninger). Kjør til det akkurat er godt blandet, og tilsett 
+ 		deretter isvann til deigen samler seg. Ikke kjør deigen mer enn det som er strengt nødvendig. Pakk deigen inn i litt 
+ 		plastfolie og legg den i kjøleskapet i minst en time (del den gjerne opp i fire biter så blir den klar for utkjevling 
+ 		tidligere).</p>
+	 <p>Ta deigen på melet bakebord og del den i 4 like store emner (eller du kan selvsagt bake en stor terte om du heller 
+ 		ønsker det). Kjevle hvert av emnene til 10-12 cm i diameter. Legg tertebunnene på bakepapirkledt stekebrett.</p>
+	 <p>Lag vaniljekrem, etter feks denne oppskriften, eller bruk ferdigkjøpt som jeg gjorde. Tertene blir selvsagt aller best om du 
+ 		lager vaniljekremen selv, men av og til er det ikke tid til det og da får nest-best duge. Fordel 2-3 ss vaniljekrem på hver 
+ 		av tertebunnene, men la det være igjen en kant på ca 2 cm ytterst. Fordel bringebærene oppå kremen. Brett deigkanten godt innover slik at den såvidt dekker de ytterste bærene.</p>
+ 	<p>Stek tertene midt i ovnen (over- og undervarme) ved ca 200 grader i ca 20-25 minutter, til tertebunnen er gyldenbrun. 
+ 		Jeg stekte mine terter i overkant av 25 minutter. Avkjøl tertene litt på rist. (Dersom du lager en stor terte må du beregne 
+ 		noe lengre steketid.)</p>
+ <img src="img/bringebærterte.jpg" width="500px" alt="terte">
+ <br>
+ <h4 id="vaniljekrem">Vaniljekrem </h4>
+ 	<ul>
+ 		<li>1 vaniljestang</li>
+ 		<li>2 dl + ½ dl melk</li>
+ 		<li>¾ dl kremfløte</li>
+ 		<li>55 g sukker</li>
+ 		<li>2 små eggeplommer</li>
+ 		<li>2 ss maisenna</li>
+ 	</ul>
+ 	<p>Ha 2 dl melk, fløte og sukker i en kjele. Splitt vaniljestangen, skrap ut frøene og tilsett frøene og stangen i kjelen. 
+ 		Kjelen bør ikke være mer enn halvfull. Varm forsiktig opp til kokepunktet.Bland sammen ½ dl melk og maisenna i en stor bolle 
+ 		og visp sammen. Visp deretter inn eggeplommene.  Hell ca 2/3 av den varme væsken, litt etter litt, oppi eggeblandingen og rør godt hele tiden med en visp. </p>
+ 	<p>Sil deretter blandingen og hell den tilbake i kjelen. Varm forsiktig opp og la blandingen småkoke (skal ikke boblekoke!) mens 
+ 		du hele tiden rører i bunnen av kjelen slik at kremen ikke brenner seg. Kremen skal nå begynne å tykne. Hell kremen over 
+ 		i en bolle, og sett den umiddelbart til avkjøling i et vannbad eller lignende. Når kremen er avkjølt dekkes bollen med plast 
+ 		og settes i kjøleskapet.</p>
+ <br>
+ <h4>Matpakkeforslag - Mango og skinkewrap</h4>
+ 	<ul>
+ 		<li>Lefse</li>
+ 		<li>Kjøtt/Skinke av eget valg</li>
+ 		<li>Roccola salat</li>
+ 		<li>Vårløk</li>
+		<li>Mango</li>
+		<li>Cherrytomater</li>
+		<li>Kesam naturel</li>
+	</ul>
+	<p>Man legger tar først å smører litt naturell kesam over lefsen, deretter legger man på den skinken man ønsker, så strør 
+		man over den salaten man har valgt, vårløken, cherrytomatene, og mangoskivene. Så bretter/ruller man det sammen som en 
+		taco og pakker det inn i plastfolie, så den holder seg fint sammen brettet/rullet til lunchen på skolen.</p>
+	<img src="img/matpakke.png" width="500px" alt="matpakke">
+<br>
+<h2>Smoothie</h2>
+<h4>Smart Smoothie (1porsjon)</h4>
+	<ul>
+		<li>3dl appelsinjuice</li>
+		<li>½ stk brokkoli</li>
+		<li>200g skogsbærblanding</li>
+		<li>6 stk isbiter</li>
+	</ul>
+	<p>Ha appelsinjuicen I en blender. Brekk opp brokkolien og ha den sammen I blenderen med skogsbærblandingen og isbitene. 
+		Kjør det hele til du har fått en jevn smoothie blanding.</p>
+	<img src="img/smart-smoothie.jpg" width="500px" alt="smart-smoothie">
+<br>
+<h4>Gulrot- og ananas smoothie (1porsjon)</h4>
+	<ul>
+		<li>2 skiver frisk ananas</li>
+		<li>1 dl appelsinjuice</li>
+		<li>1 stk gulrot</li>
+		<li>2 dl yoghurt naturell</li>
+	</ul>
+	<p>Rens og skjær ananas og gulrot I biter. Ha appelsinjuicen I mikseren først – deretter yoghurt, ananas og gulot. 
+		Kjør blandingen til en du har fått en glatt smotthie</p>
+	<img src="img/gulrot-ananassmoothie.jpg" width="500px" alt="gulrot-ananassmoothie">
+<br>
+<h4>Kilder</h4>
+	<p>Vi har hentet våre oppskrifter fra <a href="http://trinesmatblogg.no">Trines matblogg</a> </p>
+	<p>Vi har hentet vår matpakke og smoothier fra <a href="http://www.frukt.no/">Frukt.no</a></p>
+
+<!--ul
+{
+    list-style-type: none;
+}-->
