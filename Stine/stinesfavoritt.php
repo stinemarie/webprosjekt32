80168f18e5d7817bea3ec0159a8a35d6b72b7786
stinemarie <stinemarie86@dhcp-158-39-192-254.wlan.hioa.no>
2013-11-07 12:45:18 +0100

    la til granateple som min favoritt

--- Stine/stinesfavoritt.php
+++ Stine/stinesfavoritt.php
@@ -1,10 +1,9 @@
-<h2>Våre favoritter</h2>
-
-<h4> Stines Favoritt: Granateple </h4>
+<h4> Stines Favoritt!</h4>
+<h5> Granateple </h5>
 
  <figure>
   <img src="./img/pomegranate200x300.jpg" alt="bilde av granateple">
-  <figcaption class="kilde">kilde: Wikipedia foto: Anton Croos</figcaption>
+  <figcaption>kilde: Wikipedia foto: Anton Croos</figcaption>
 </figure> 
 
 <p> 
@@ -13,8 +12,8 @@
 	antioksidanter enn appelsiner og nesten 40 ganger mer enn epler. Både 
 	håndgranater og byen Granada i Spania er oppkalt etter frukten. Den passer bra til å ha i 
 	salat. Da gjerne med frisk salat, avokado, feta ost, rødløk og cherrytomater 
-	nam nam.<br> PS! super digg å ha i daiquiri også! 
+	nam nam. ps! super digg å ha i daiquiri også! 
 </p>
 
-<p class="kilde"> Kilde: <a href="http://en.wikipedia.org/wiki/Pomegranate" target="_blank"> Wikipedia</a> og <a href="http://www.bama.no" target="_blank"> bama.no</a> </p>
+<q> Kilde: <a href="http://en.wikipedia.org/wiki/Pomegranate"> Wikipedia</a> og <a href="http://www.bama.no"> bama.no</a> </q>
 
