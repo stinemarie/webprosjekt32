f302bd764a6f7c0539d36525ff26bb85175c126a
stinemarie <stinemarie86@dhcp-158-39-192-254.wlan.hioa.no>
2013-11-07 18:47:36 +0100

    fikset feil i .kar sin js

--- Websiden/pages/hjem.php
+++ Websiden/pages/hjem.php
@@ -1,12 +1,45 @@
 <h2>Hjem</h2>
 
+<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+	magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+	 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</p>
 <nav class="kar">
 	<div id="ramme">
 		<ul>
-			<li><div id="alpha"><p>trening</p><a href=""><img src="./img/fisk1.jpg"></a></div></li>
-			<li><div id="beta"><p>kosthold</p><a href=""><img src="./img/suppe1.jpg"></a></div></li>
-			<li><div id="gamma"><p>helse</p><a href=""><img src="./img/smoothie1.jpg"></a></div></li>
-			<li><div id="delta"><p>personlig trener</p><a href=""><img src="./img/terte1.jpg"></a></div></li>
+			<li>
+				<div id="alpha">
+					<p> les mer om trening</p>
+					<a href="">
+						<img src="./img/fisk1.jpg">
+					</a>
+				</div>
+			</li>
+			<li>
+				<div id="beta">
+					<p>les mer om kosthold</p>
+					<a href="">
+						<img src="./img/suppe1.jpg">
+					</a>
+				</div>
+			</li>
+			<li>
+				<div id="gamma">
+					<p>les mer om helse</p>
+					<a href="">
+						<img src="./img/smoothie1.jpg">
+					</a>
+				</div>
+			</li>
+			<li>
+				<div id="delta">
+					<p>les mer om personlig trener</p>
+					<a href="">
+						<img src="./img/terte1.jpg">
+					</a>
+				</div>
+			</li>
 		</ul>
 				<div id="tilbake"></div><div id="neste"></div>
 	</div><!--  slutt på ramme -->
--- Websiden/script.js
+++ Websiden/script.js
@@ -122,87 +122,84 @@ $(document).ready(function() {
 			$("#omNav").css("display","none");
 		}, 3000);
 	});
-
-
-
 });
 
 /*Kode for bildekarusell på førstesiden*/
+
 $(document).ready(function(){
 	 $("#alpha")
-	 .css("width","200px")
-	 .css("height","200px")
-	 .css("z-index","2")
-	 .css("left","10px")
-	 .css("top","20px")
-	 .addClass('left'); 
+	  .addClass('left'); 
 	 $("#beta")
-	 .css("width","230px")
-	 .css("height","230px")
-	 .css("z-index","6")
-	 .css("left","175px")
-	 .css("top","10px")
 	 .addClass('midten'); 
 	 $("#gamma")
-	 .css("width","200px")
-	 .css("height","200px")
-	 .css("z-index","3")
-	 .css("left","370px")
-	 .css("top","20px")
 	 .addClass('right');
 	 $("#delta")
-	 .css("width","200px")
-	 .css("height","200px")
-	 .css("z-index","1")
-	 .css("left","170px")
-	 .css("top","20px")
 	 .addClass('hide');
 	 $( '.midten img').css("width","230px");
 	 $( '.midten img').css("height","230px");
+	 $( '.midten p').fadeIn('fast');
+	 $( "#neste" ).click(function(){
+	 	neste();
+	 });
 
-	 $("#neste").click(function(){
-	 	var rrr = $("div").index( $(".right") );
-	 	var mmm = $("div").index( $(".midten") );
-	 	var lll = $("div").index( $(".left") );
-	 	var hhh = $("div").index( $(".hide") );
+// 	 $('#button').click(function(){
+//   if(
+//     $('.wrapper').not(':animated') && $('.wrapper span').is(':visible')
+//   ) {
+//     //do something
+//   }
+// })
+
+
+		
+	setInterval(function() {neste()}, 6500);
+
+	
+
+	function neste(){
+	 	var rrr = $(".kar div").index( $(".right") );
+	 	var mmm = $(".kar div").index( $(".midten") );
+	 	var lll = $(".kar div").index( $(".left") );
+	 	var hhh = $(".kar div").index( $(".hide") );
+
+	 	$( "#tilbake" ).css("display","none").delay( 300 ).fadeIn("4000");
+	 	$( "#neste" ).css("display","none").delay( 300 ).fadeIn("4000");
+	 	
+	 	$( ".kar div" ).eq(rrr).css("z-index","6").animate({left:'170px', top:'10px'},"slow").addClass('midten');
+	 	$( ".kar div" ).eq(rrr).removeClass('right');
+		$( ".kar div" ).eq(mmm).css("z-index","3").animate({left:'10px', top:'20px'},"slow").addClass('left');
+	 	$( ".kar div" ).eq(mmm).removeClass('midten');
+	 	$( ".kar div" ).eq(lll).css("z-index","2").animate({left:"160px", top:'20px',  width:'200px', height:'200px'}).addClass('hide');
+	 	$( '.kar div' ).eq(lll).removeClass('left');
+	 	$( ".kar div" ).eq(hhh).css("z-index","1").animate({left:'370px', top:'20px',},"slow").addClass('right');
+	 	$( '.kar div' ).eq(hhh).removeClass('hide');
+
+	 	$( '.midten img').animate({width:'230px', height:'230px'});
+	 	$( '.left img' ).animate({width:'200px', height:'200px'});
+	 	// $( '.left p').fadeOut('fast');					 			
+	 };
+	 	$("#tilbake").click(function(){
+	 	var rrr = $(".kar div").index( $(".right") );
+	 	var mmm = $(".kar div").index( $(".midten") );
+	 	var lll = $(".kar div").index( $(".left") );
+	 	var hhh = $(".kar div").index( $(".hide") );
 
 	 	$( "#tilbake" ).css("display","none").delay( 300 ).fadeIn("4000");
 	 	$( "#neste" ).css("display","none").delay( 300 ).fadeIn("4000");
 
-	 	$( "div" ).eq(rrr).css("z-index","1").animate({left:"170px", top:'20px'}).addClass('hide');
-	 	$( "div" ).eq(rrr).removeClass('right');
-		$( "div" ).eq(mmm).css("z-index","3").animate({left:'370px', top:'20px'},"slow").addClass('right');
-	 	$( "div" ).eq(mmm).removeClass('midten');
-	 	$( "div" ).eq(lll).css("z-index","6").animate({left:'175px', top:'10px'},"slow").addClass('midten');
-	 	$( 'div' ).eq(lll).removeClass('left');
-	 	$( "div" ).eq(hhh).css("z-index","2").animate({left:'10px', top:'20px'},"slow").addClass('left');
-	 	$( 'div' ).eq(hhh).removeClass('hide');	
-
-	 	$( '.midten img').delay( 75 ).animate({width:'230px', height:'230px'});
-	 	$( '.right img' ).delay( 75 ).animate({width:'200px', height:'200px'});					 			
+	 	$( ".kar div" ).eq(rrr).css("z-index","1").animate({left:"170px", top:'20px'}).addClass('hide');
+	 	$( ".kar div" ).eq(rrr).removeClass('right');
+		$( ".kar div" ).eq(mmm).css("z-index","3").animate({left:'370px', top:'20px'},"slow").addClass('right');
+	 	$( ".kar div" ).eq(mmm).removeClass('midten');
+	 	$( ".kar div" ).eq(lll).css("z-index","6").animate({left:'175px', top:'10px'},"slow").addClass('midten');
+	 	$( '.kar div' ).eq(lll).removeClass('left');
+	 	$( ".kar div" ).eq(hhh).css("z-index","2").animate({left:'10px', top:'20px'},"slow").addClass('left');
+	 	$( '.kar div' ).eq(hhh).removeClass('hide');	
+			
+	 	$( '.midten img').animate({width:'230px', height:'230px'});
+	 	$( '.right img' ).animate({width:'200px', height:'200px'});	
+	 	// $( '.left p').fadeOut('fast');	
 	 });
-	 	$("#tilbake").click(function(){
-	 	var rrr = $("div").index( $(".right") );
-	 	var mmm = $("div").index( $(".midten") );
-	 	var lll = $("div").index( $(".left") );
-	 	var hhh = $("div").index( $(".hide") );
-		
-		$( "#tilbake" ).css("display","none").delay( 300 ).fadeIn("4000");
-	 	$( "#neste" ).css("display","none").delay( 300 ).fadeIn("4000");
-	 	
-	 	$( "div" ).eq(rrr).css("z-index","6").animate({left:'170px', top:'10px'},"slow").addClass('midten');
-	 	$( "div" ).eq(rrr).removeClass('right');
-		$( "div" ).eq(mmm).css("z-index","3").animate({left:'10px', top:'20px'},"slow").addClass('left');
-	 	$( "div" ).eq(mmm).removeClass('midten');
-	 	$( "div" ).eq(lll).css("z-index","2").animate({left:"160px", top:'20px',  width:'200px', height:'200px'}).addClass('hide');
-	 	$( 'div' ).eq(lll).removeClass('left');
-	 	$( "div" ).eq(hhh).css("z-index","1").animate({left:'370px', top:'20px',},"slow").addClass('right');
-	 	$( 'div' ).eq(hhh).removeClass('hide');
-	 	
-	 	$( '.midten img').delay( 75 ).animate({width:'230px', height:'230px'});
-	 	$( '.left img' ).delay( 75 ).animate({width:'200px', height:'200px'});		
-	 });
-
 });
 
 /*Skript for bildegallerier*/
