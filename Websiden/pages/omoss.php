5ec52c0896e997dcaec5318b828a36d3ac67dec4
amundring <amund@dhcp-158-39-198-28.wlan.hioa.no>
2013-11-10 15:01:01 +0100

    "Om oss"-siden

--- Websiden/pages/omoss.php
+++ Websiden/pages/omoss.php
@@ -1,7 +1,47 @@
 <h2>Om oss</h2>
 
-<img src="./img/stine.jpg" alt="portrettfoto Stine MA Grumheden">
-<h4>Stine Marie Aas Grumheden</h4>
-<p>e-post: stinemarie86@gmail.com</p>
-<a href="http://student.cs.hioa.no/~s193467/webprosjekt2/
-" target="_blank" >besøk min hjemmeside</a>
+<p id="omosstekst">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+mollit anim id est laborum:</p>
+
+<div id="bilderavoss">
+	<figure class="omossfigure">
+		<img src="./img/stine.jpg" width="200px" height="200px" alt="Bilde av Stine">
+		<figcaption>
+			Stine Marie Aas Grumheden
+			<br>
+			<a id="mail" href="mailto:stinemarie86@gmail.com">stinemarie86@gmail.com</a>
+		</figcaption>
+	</figure>
+
+	<figure class="omossfigure">
+		<img src="./img/amund.jpg" width="200px" height="200px" alt="Bilde av Amund">
+		<figcaption>
+			Amund Ring
+			<br>
+			<a id="mail" href="mailto:dnuma88@gmail.com">dnuma88@gmail.com</a>
+		</figcaption>
+	</figure>
+
+	<figure class="omossfigure">
+		<img src="./img/ida.png" width="200px" height="200px" alt="Bilde av Ida">
+		<figcaption>
+			Ida Vikaskag Salvesen
+			<br>
+			<a id="mail" href="mailto:ida@gmail.com">idaida@gmail.com</a>
+		</figcaption>
+	</figure>
+
+	<figure class="omossfigure">
+		<img src="./img/christopher.png" width="200px" height="200px" alt="Bilde av Christopher">
+		<figcaption>
+			Christopher Aspel
+			<br>
+			<a id="mail" href="mailto:christopher@gmail.com">christopher@gmail.com</a>
+		</figcaption>
+	</figure>
+</div>
