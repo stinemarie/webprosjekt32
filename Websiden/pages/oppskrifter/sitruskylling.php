354539e56e179523d58048c14af36b62a72c5041
idaings <IdaVikaskagSalvesen@Ida-Vikaskag-Salvesens-MacBook-Pro.local>
2013-11-10 19:57:50 +0100

    Lastet opp flere ferdige oppskrifter
    De som hører til ukesmenyen, med bilder til (som er lagt i img mappen)
    og lagt til bilde på oppskrift.php filen for de nye oppskriftene

---
+++ Websiden/pages/oppskrifter/sitruskylling.php
@@ -0,0 +1,31 @@
+<h4>Sitruskylling med asparges og timian (2porsjoner)</h4>
+<ul>
+	<li>2 Kyllingfileter</li>
+	<li>Salt og pepper</li>
+	<li>2 fedd Hvitøk, finhakket</li>
+	<li>1/2 Sitron, saft og skall</li>
+	<li>1/2 Appelsin, saft og skall</li>
+	<li>Frisk timian, hakket</li>
+	<li>2 Klatter smør</li>
+	<li>1 Sitron, delt i to</li>
+	<li>8 Asparges</li>
+	<li>Litt sukker og salt</li>
+</ul>
+<p>Krydre kyllingfiletene med salt og pepper og brun raskt på begge sider. Stek den delte sitronen sammen med kyllingen, med snittsiden 
+	ned. Legg filetene i et lite ildfast fat og hell appelsin- og sitronsaften over, og fordel finhakket hvitløk, sitrusskall, smør og 
+	hakket timian over. Legg sitronene oppi formen, med snittsiden opp. Stek kylling og sitronen ved ca 200 grader i ca 10-15 minutter, til 
+	kyllingen er gjennomstekt. La kyllingen hvile i ca 5 minutter før servering. Ta vare på sitrussjyen og ha den i en liten sausemugge.</p>
+<p>Kok aspargesen 2-3 minutter i rikelig med vann tilsatt litt salt og sukker. Ha den ferdigkokte aspargesen i kaldt vann slik at du får 
+	stanset kokeprosessen.</p>
+<p>Anrett aspargesen på en tallerken og legg kyllingfileten oppå. Drypp litt av sitrussjyen over og pynt med frisk timian. Serveres med resten 
+	av sitrussjyen, brød, ris eller annet tilbehør.</p>
+
+
+	<figure>
+	<img src="img/sitruskylling.jpg" width="500px" alt="sitruskylling">
+		<figcaption class="kilde">Kilde: Trines Matblogg Foto:</figcaption>
+	</figure> 
+
+	<p class="kilde">Kilde: <a href="www.trinesmatblogg.no" target="_blank">Trines Matblogg</a></p>
+
+ 	<a href="index.php?page=oppskrifter">tilbake</a>
---
+++ Websiden/pages/oppskrifter/sommersalat.php
@@ -0,0 +1,31 @@
+<h4>Sommersalat med jordbær, eple, kraftkar og tind spekeskinke (3porsjoner)</h4>
+<ul>
+	<li>1 pakke Tind godt modnet spekeskinke</li>
+	<li>ca. 100 g Blandet salat</li>
+	<li>1/2 Rødløk, i tynne båter</li>
+	<li>1/2 beger Cherrytomater, delt i to</li>
+	<li>1/2 beger Jordbær, delt i to</li>
+	<li>1/2 Eple, i tynne båter</li>
+	<li>Kraftkar eller annen god blåmuggost, i biter</li>
+	<li>2-3 ss Gresskarkjerner, ristet i tørr panne</li>
+	<li>ca. 4 ss God olivenolje</li>
+	<li>ca. 2 ss God balsamicoeddik</li>
+	<li>Salt og pepper</li>
+	<li>Godt brød, for eksempel focaccia</li>
+</ul>
+<p>Rist gresskarkjernene i en tørr panne og sett til side.</p>
+<p>Skyll salaten og kjør den gjerne i en salatslynge, hvis du har. Skjær opp grønnsaker og frukt som angitt ovenfor, 
+	og ha dem i en stor bolle sammen med salaten og de ristede gresskarkjernene. Drypp over din beste olivenolje og 
+	balsamicoeddik, og krydre med salt og pepper. Vend salaten forsiktig sammen, og smuldre til slutt osten over.</p>
+<p>Serveres umiddelbart, sammen med godt brød til. Sett gjerne frem en lav bolle med olivenolje og balsamicoeddik som man 
+	kan dyppe brødet i.</p>
+
+
+	<figure>
+	<img src="img/sommersalat.jpg" width="500px" alt="sommersalat">
+		<figcaption class="kilde">Kilde: Trines Matblogg Foto:</figcaption>
+	</figure> 
+
+	<p class="kilde">Kilde: <a href="www.trinesmatblogg.no" target="_blank">Trines Matblogg</a></p>
+
+ 	<a href="index.php?page=oppskrifter">tilbake</a>
