ce7d86742754b7f48655fb379dcde4960aa65d07
idaings <IdaVikaskagSalvesen@Ida-Vikaskag-Salvesens-MacBook-Pro.local>
2013-11-11 00:38:52 +0100

    Start på Før/Etter prosjekt tekst

--- Websiden/pages/treningsprosjekt.php
+++ Websiden/pages/treningsprosjekt.php
@@ -1,1 +1,135 @@
-<h2>Før- og etterprosjekt</h2>
+<h2>Før/Etter med TTFS</h2>
+
+<p>Vi har under prosjektperioden gått inn for å ha et Før og Etter prosjekt, som Ida og Christopher har utført. Her satte begge to seg noen
+	små oppnåelige mål for den korte perioden vi hadde, og så skulle vi se hvilke av disse målene som vi følte at vi klarte oppfylle etter
+	at prosjektperioden var over.</p>
+
+<h4>Ida</h4>
+<img src="" alt="Ida Før prosjektet">
+<p>Jeg følte at for meg var det ikke å oppnå å gå ned så, så mange kilo på den ene måneden som vi skulle ha dette prosjektet. For meg så gikk
+	det mer ut på å få gjennomført de øktene som jeg vet skal til for at jeg skal kunne sitte igjen med en god selvfølelse. 
+	Mine mål ble derfor:</p>
+	<ul>
+		<li>Få trent minst 3 ganger hver uke</li>
+		<li>I disse øktene skulle jeg ha minimum 2 intervaller og 2 styrkeøkter fordelt</li>
+		<li>Unngå kollektiv</li>
+	</ul>
+<!--><img src="" alt="Før prosjektet"><!-->
+
+<p>Nå som vi er ferdig med prosjektet, så kan jeg si at jeg har klart å følge alle mine mål, unntatt den ene uken, da jeg var syk, og kun
+fikk gjort to av tre ønskede økter den uken.</p>
+<img src="" alt="Ida Etter prosjektet">
+
+<h4>Christopher</h4>
+<img src="" alt="Christopher Før prosjektet">
+<p></p>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+<!--><img src="" alt="Christopher Før prosjektet"><!-->
+
+<p></p>
+<img src="" alt="Christopher Etter prosjektet">
+
+
+<h2>Skjema over utførelse</h2>
+
+<table border="1">
+	<tr>
+		<th>Ida</th>
+		<th>Christopher</th>
+	</tr>
+	<tr>
+	</tr>
+	<tr>
+		<td>
+			<table border="0">
+			<tr>
+				<th>Dato</th>
+				<th>Øvelse</th>
+				<th>Rep./Ser.</th>
+			</tr>
+			<tr>
+				<td>Row 1, cell 1</td>
+				<td>Row 1, cell 2</td>
+				<td>Row 1, cell 3</td>
+			</tr>
+			</table>
+		</td>
+		<td>	
+			<table border="0">
+			<tr>
+				<th>Dato</th>
+				<th>Øvelse</th>
+				<th>Rep./Ser.</th>
+			</tr>
+			<tr>
+				<td>Row 2, cell 1</td>
+				<td>Row 2, cell 2</td>
+				<td>Row 2, cell 3</td>
+			</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<table border="0">
+			<tr>
+				<td>Row 1, cell 1</td>
+				<td>Row 1, cell 2</td>
+				<td>Row 1, cell 3</td>
+			</tr>
+			</table>
+		</td>
+		<td>	
+			<table border="0">
+			<tr>
+				<td>Row 2, cell 1</td>
+				<td>Row 2, cell 2</td>
+				<td>Row 2, cell 3</td>
+			</tr>
+			</table>
+		</td>
+	</tr>
+		<tr>
+		<td>
+			<table border="0">
+			<tr>
+				<td>Row 1, cell 1</td>
+				<td>Row 1, cell 2</td>
+				<td>Row 1, cell 3</td>
+			</tr>
+			</table>
+		</td>
+		<td>	
+			<table border="0">
+			<tr>
+				<td>Row 2, cell 1</td>
+				<td>Row 2, cell 2</td>
+				<td>Row 2, cell 3</td>
+			</tr>
+			</table>
+		</td>
+	</tr>
+		<tr>
+		<td>
+			<table border="0">
+			<tr>
+				<td>Row 1, cell 1</td>
+				<td>Row 1, cell 2</td>
+				<td>Row 1, cell 3</td>
+			</tr>
+			</table>
+		</td>
+		<td>	
+			<table border="0">
+			<tr>
+				<td>Row 2, cell 1</td>
+				<td>Row 2, cell 2</td>
+				<td>Row 2, cell 3</td>
+			</tr>
+			</table>
+		</td>
+	</tr>
