3d19c2f0c4db6c10d9a4a77cfbff5d06b2ce885c
idaings <IdaVikaskagSalvesen@Ida-Vikaskag-Salvesens-MacBook-Pro.local>
2013-11-11 14:36:26 +0100

    Fylt inn treningssentre på "hvor man kan trene"

--- Websiden/pages/hvor.php
+++ Websiden/pages/hvor.php
@@ -1,1 +1,19 @@
 <h2>Hvor kan man trene?</h2>
+
+<h4>Elixia</h4>
+	<a href="http://www.elixia.no">Elixia</a>
+
+<h4>SATS</h4>
+	<a href="http://www.sats.no">SATS</a>
+
+<h4>EVO</h4>
+	<a href="https://www.evofitness.no">EVO</a>
+
+<h4>Fitness 24seven</h4>
+	<a href="http://www.fitness24seven.com/no/start">Fitness 24seven</a>
+
+<h4>Fresh fitness</h4>
+	<a href="http://www.freshfitness.no">Fresh fitness</a>
+
+<h4>Fitness xpress</h4>
+	<a href="http://www.fitnessxpress.no">Fitness xpress</a>
