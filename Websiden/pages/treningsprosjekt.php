a4c39fc2dbbc23e8b95577b7c85fc927788c3f1a
idaings <IdaVikaskagSalvesen@Ida-Vikaskag-Salvesens-MacBook-Pro.local>
2013-11-11 14:11:52 +0100

    Oppdatering treningsprosjekt.php

--- Websiden/pages/treningsprosjekt.php
+++ Websiden/pages/treningsprosjekt.php
@@ -1,136 +1,168 @@
+<meta charset="utf-8">
 <h2>Før/Etter med TTFS</h2>
 
-<p>Vi har under prosjektperioden gått inn for å ha et Før og Etter prosjekt, som Ida og Christopher har utført. Her satte begge to seg noen
-	små oppnåelige mål for den korte perioden vi hadde, og så skulle vi se hvilke av disse målene som vi følte at vi klarte oppfylle etter
-	at prosjektperioden var over.</p>
-
+	<p>Vi har under prosjektperioden gått inn for å ha et Før og Etter prosjekt, som Ida og Christopher har utført. Her satte begge to seg noen
+		små oppnåelige mål for den korte perioden vi hadde, og så skulle vi se hvilke av disse målene som vi følte at vi klarte oppfylle etter
+		at prosjektperioden var over.</p>
 <h4>Ida</h4>
-<img src="" alt="Ida Før prosjektet">
-<p>Jeg følte at for meg var det ikke å oppnå å gå ned så, så mange kilo på den ene måneden som vi skulle ha dette prosjektet. For meg så gikk
-	det mer ut på å få gjennomført de øktene som jeg vet skal til for at jeg skal kunne sitte igjen med en god selvfølelse. 
-	Mine mål ble derfor:</p>
+	<img src="" alt="Ida Før prosjektet">
+	<p>Jeg følte at for meg var det ikke å oppnå å gå ned så, så mange kilo på den ene måneden som vi skulle ha dette prosjektet. For meg så 	gikk det mer ut på å få gjennomført de øktene som jeg vet skal til for at jeg skal kunne sitte igjen med en god selvfølelse. 
+		Mine mål ble derfor:</p>
 	<ul>
 		<li>Få trent minst 3 ganger hver uke</li>
 		<li>I disse øktene skulle jeg ha minimum 2 intervaller og 2 styrkeøkter fordelt</li>
 		<li>Unngå kollektiv</li>
 	</ul>
-<!--><img src="" alt="Før prosjektet"><!-->
-
-<p>Nå som vi er ferdig med prosjektet, så kan jeg si at jeg har klart å følge alle mine mål, unntatt den ene uken, da jeg var syk, og kun
-fikk gjort to av tre ønskede økter den uken.</p>
-<img src="" alt="Ida Etter prosjektet">
-
-<h4>Christopher</h4>
-<img src="" alt="Christopher Før prosjektet">
-<p></p>
-	<ul>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-<!--><img src="" alt="Christopher Før prosjektet"><!-->
-
-<p></p>
-<img src="" alt="Christopher Etter prosjektet">
-
-
+	<p>Nå som vi er ferdig med prosjektet, så kan jeg si at jeg har klart å følge alle mine mål, unntatt den ene uken, da jeg var syk, og kun
+	fikk gjort to av tre ønskede økter den uken.</p>
+		<img src="" alt="Ida Etter prosjektet">
 <h2>Skjema over utførelse</h2>
 
 <table border="1">
 	<tr>
-		<th>Ida</th>
-		<th>Christopher</th>
+		<th>Dato</th>
+		<th>Øvelse</th>
+		<th>Kg</th>
+		<th>Rep</th>
+		<th>Serier</th>
 	</tr>
 	<tr>
+		<td>27.10.13</td>
+		<td>Intervall 15x15</td>
+		<td>X</td>
+		<td>X</td>
+		<td>30 min</td>
 	</tr>
 	<tr>
-		<td>
-			<table border="0">
-			<tr>
-				<th>Dato</th>
-				<th>Øvelse</th>
-				<th>Rep./Ser.</th>
-			</tr>
-			<tr>
-				<td>Row 1, cell 1</td>
-				<td>Row 1, cell 2</td>
-				<td>Row 1, cell 3</td>
-			</tr>
-			</table>
-		</td>
-		<td>	
-			<table border="0">
-			<tr>
-				<th>Dato</th>
-				<th>Øvelse</th>
-				<th>Rep./Ser.</th>
-			</tr>
-			<tr>
-				<td>Row 2, cell 1</td>
-				<td>Row 2, cell 2</td>
-				<td>Row 2, cell 3</td>
-			</tr>
-			</table>
-		</td>
+		<td>27.10.13</td>
+		<td>Sideløft (mage)</td>
+		<td>10 kg</td>
+		<td>30</td>
+		<td>3</td>
 	</tr>
 	<tr>
-		<td>
-			<table border="0">
-			<tr>
-				<td>Row 1, cell 1</td>
-				<td>Row 1, cell 2</td>
-				<td>Row 1, cell 3</td>
-			</tr>
-			</table>
-		</td>
-		<td>	
-			<table border="0">
-			<tr>
-				<td>Row 2, cell 1</td>
-				<td>Row 2, cell 2</td>
-				<td>Row 2, cell 3</td>
-			</tr>
-			</table>
-		</td>
+		<td>27.10.13</td>
+		<td>TRX mage</td>
+		<td>X</td>
+		<td>12</td>
+		<td>3</td>
 	</tr>
-		<tr>
-		<td>
-			<table border="0">
-			<tr>
-				<td>Row 1, cell 1</td>
-				<td>Row 1, cell 2</td>
-				<td>Row 1, cell 3</td>
-			</tr>
-			</table>
-		</td>
-		<td>	
-			<table border="0">
-			<tr>
-				<td>Row 2, cell 1</td>
-				<td>Row 2, cell 2</td>
-				<td>Row 2, cell 3</td>
-			</tr>
-			</table>
-		</td>
+	<tr>
+		<td>30.10.13</td>
+		<td>Markløft</td>
+		<td>45 kg</td>
+		<td>12</td>
+		<td>3</td>
 	</tr>
-		<tr>
-		<td>
-			<table border="0">
-			<tr>
-				<td>Row 1, cell 1</td>
-				<td>Row 1, cell 2</td>
-				<td>Row 1, cell 3</td>
-			</tr>
-			</table>
-		</td>
-		<td>	
-			<table border="0">
-			<tr>
-				<td>Row 2, cell 1</td>
-				<td>Row 2, cell 2</td>
-				<td>Row 2, cell 3</td>
-			</tr>
-			</table>
-		</td>
+	<tr>
+		<td>30.10.13</td>
+		<td>Roing, kabelmaskin</td>
+		<td>30 kg</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	<tr>
+		<td>30.10.13</td>
+		<td>Vektløft (hver arm)</td>
+		<td>8 kg</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	<tr>
+		<td>30.10.13</td>
+		<td>Situps bosu ball</td>
+		<td>6 kg</td>
+		<td>15</td>
+		<td>3</td>
+	</tr>
+		<td>30.10.13</td>
+		<td>Planke medisin ball</td>
+		<td>X</td>
+		<td>10</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td>30.10.13</td>
+		<td>Sideløft (mage)</td>
+		<td>10 kg</td>
+		<td>30</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td>30.10.13</td>
+		<td>TRX mage</td>
+		<td>X</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td><strong>03.11.13</strong></td>
+		<td><strong>Falt</strong></td>
+		<td><strong>bort</strong></td>
+		<td><strong>pga</strong></td>
+		<td><strong>syk</strong></td>
+	</tr>
+	</tr>
+		<td>06.11.13</td>
+		<td>Intervall 4x4</td>
+		<td>X</td>
+		<td>X</td>
+		<td>40 min</td>
+	</tr>
+	</tr>
+		<td>06.11.13</td>
+		<td>Utfall, gående</td>
+		<td>8 kg (hver arm)</td>
+		<td>20</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td>06.11.13</td>
+		<td>Roing, kabelmaskin</td>
+		<td>35 kg</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td>08.11.13</td>
+		<td>Intervall, i bakke</td>
+		<td>X</td>
+		<td>X</td>
+		<td>30min</td>
+	</tr>
+	</tr>
+		<td>08.11.13</td>
+		<td>Situps</td>
+		<td>5 kg</td>
+		<td>15</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td>08.11.13</td>
+		<td>Pushups</td>
+		<td>X</td>
+		<td>15</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td>08.11.13</td>
+		<td>Planken</td>
+		<td>X</td>
+		<td>3</td>
+		<td>2 min</td>
+	</tr>
+	</tr>
+		<td>08.11.13</td>
+		<td>Spensthopp</td>
+		<td>X</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</tr>
+		<td>10.11.13</td>
+		<td>Joggetur (Bergen)</td>
+		<td>X</td>
+		<td>X</td>
+		<td>1 t 20 min</td>
 	</tr>
 </table>
