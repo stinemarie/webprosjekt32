9ed920683038b455d11c5ee53c854c467a3b0abe
amundring <amund@Amunds-MacBook-Pro-2.local>
2013-11-13 15:23:53 +0100

    Brokkoli-tekst

--- Websiden/pages/favoritter.php
+++ Websiden/pages/favoritter.php
@@ -3,16 +3,16 @@
 
 
 	<div class="favorittBoks">
-		<h4>Stines Favoritt: Granateple</h4>
+		<h4>Stines favoritt: Granateple</h4>
 		<img src="./img/pomegranate200x300.jpg" alt="bilde av granateple">
-		<p> Min favoritt er granateple. Den er proppet med antioksidanter. I følge <a href="http://www.bama.no"> bama.no</a>  inneholder den 10 ganger mer antioksidanter enn appelsiner og nesten 40 ganger mer enn epler. Både håndgranater og byen Granada i Spania er oppkalt etter frukten. Den passer bra til å ha i salat. Da gjerne med frisk salat, avokado, feta ost, rødløk og cherrytomater nam nam.<br> PS! super digg å ha i daiquiri også! </p>
-		<p class="kilde"> Kilde: <a href="http://en.wikipedia.org/wiki/Pomegranate" target="_blank"> Wikipedia</a> og <a href="http://www.bama.no" target="_blank"> bama.no</a> </p>
+		<p> Min favoritt er granateple. Den er proppet med antioksidanter. I følge <a href="http://www.bama.no"> bama.no</a>  inneholder den 10 ganger mer antioksidanter enn appelsiner og nesten 40 ganger mer enn epler. Både håndgranater og byen Granada i Spania er oppkalt etter frukten. Den passer bra til å ha i salat. Da gjerne med frisk salat, avokado, feta ost, rødløk og cherrytomater. Nam nam!<br> PS! Superdigg å ha i daiquiri også! </p>
+		<p class="kilde">Kilde: <a href="http://en.wikipedia.org/wiki/Pomegranate" target="_blank"> Wikipedia</a> og <a href="http://www.bama.no" target="_blank"> bama.no</a> </p>
 	</div>
 
 	<div class="favorittBoks">
 		<h4>Ida sin favoritt: Avokado</h4>
 		<img src="./img/avokado.jpg" alt="bilde av avokado">
-		<p>Den er rik på kalium, proteiner og B-, E-, K-, A- og C-vitaminer.  I tillegg inneholder frukten kolesterolsenkende stoffer og 
+		<p>Den er rik på kalsium, proteiner og B-, E-, K-, A- og C-vitaminer.  I tillegg inneholder frukten kolesterolsenkende stoffer og 
 		antioksidanter som er spesielt bra for nervesystemet.</p>
 		<p>Avokado vokser på trær og er en frukt, men siden den verken er søt eller inneholder fruktsyre blir den ofte oppfattet som en grønnsak. Smaken er mild, nøtteaktig og behagelig. Den høstes umoden, slik at den skal tåle transport og modnes underveis.</p>
 		<p>Varianter av avokado:</p>
@@ -25,19 +25,19 @@
 		<p>Jeg personlig elsker å mose/skjære opp avokadoen å ha den på et grovt knekkebrød, så strø litt havsalt og svart knernet pepper over, og til slutt strø over ruccula, nam nam! Hvis ikke er det jo super godt å lage en god salat, eller en guacamole med litt for mye chili.</p>
 		<p class="kilde"> Kilde: <a href="http://www.frukt.no/leksikon/gronnsaker/avokado/" target="_blank"> Frukt.no</a>, 
 		<a href="http://www.kriweb.no/2013/02/14/kjottkaker-med-avokado/" target="_blank"> kriweb.no</a> og 
-		<a href="https://itunes.apple.com/gb/app/z-food-nutrition-facts-lite/id484101503?mt=8" target="_blank"> Apple app: A-Z Food Nutrition Facts lite.</a> </p>
-		<p class="kilde"> Kilde: <a href="http://en.wikipedia.org/wiki/Pomegranate" target="_blank"> Wikipedia</a> og <a href="http://www.bama.no" target="_blank"> bama.no</a> </p>
+		<a href="https://itunes.apple.com/gb/app/z-food-nutrition-facts-lite/id484101503?mt=8" target="_blank"> Apple app: A-Z Food Nutrition Facts lite</a> </p>
 	</div>
 
 	<div class="favorittBoks">
-		<h4>Amund sin Favoritt: Brokkoli</h4>
+		<h4>Amund sin favoritt: Brokkoli</h4>
 		<img src="./img/brokkoli.jpg" alt="bilde av brokkoli">
-		<p>Her kommer min brokkoli-tekst</p>
-		<p class="kilde">Her kommer kildene</p>
+		<p>Min favoritt-grønnsak er brokkoli. En av grunnene til at jeg liker denne grønnsaken godt er at den er enkel å tilberede og kan brukes i veldig mange sammenhenger. Brokkoli er en av de mest næringsrike grønnsakene som finnes, og den er full av antioksidanter!
+		Selv liker jeg å kutte opp brokkolien i passe store biter og la de steke i litt olivenolje et par minutter. Den kan også dampes, kokes, stekes i ovn eller rett og slett bare spises som den er!</p>
+		<p class="kilde">Kilde: <a href="http://www.bama.no" target="_blank"> bama.no</a></p>
 	</div>
 
 		<div class="favorittBoks">
-		<h4>Christopher sin Favoritt: Asparges</h4>
+		<h4>Christopher sin favoritt: Asparges</h4>
 		<img src="./img/asparges.jpg" alt="bilde av asparges">
 		<p>Her kommer Christopher sin asparges-tekst</p>
 		<p class="kilde">Her kommer kildene</p>
