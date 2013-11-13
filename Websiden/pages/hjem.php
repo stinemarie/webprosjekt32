de10ae56e92a630f83c553e313f1e18820a44286
amundring <amund@Amunds-MacBook-Pro-2.local>
2013-11-13 15:02:45 +0100

    Litt styling på forsiden

--- Websiden/pages/hjem.php
+++ Websiden/pages/hjem.php
@@ -1,7 +1,9 @@
 <div class="hjem">	
-	<h2>Hjem</h2>
 
-	<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+	<p id="hjemtekst">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+	
+	<hr>
+
 	<nav class="kar">
 		<div id="ramme">
 			<ul>
@@ -44,5 +46,8 @@
 			</ul>
 			<div id="tilbake"></div><div id="neste"></div>
 		</div><!--  slutt på ramme -->
-	</nav>	
+	</nav>
+
+	<hr id="hrnede">
+
 </div>
