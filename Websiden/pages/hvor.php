3e047622caca79f2756ca7df4973aba77044fb5d
idaings <IdaVikaskagSalvesen@dhcp-158-39-193-244.wlan.hioa.no>
2013-11-13 10:02:54 +0100

    hvor man trener oppdatering

--- Websiden/pages/hvor.php
+++ Websiden/pages/hvor.php
@@ -1,19 +1,104 @@
 <h2>Hvor kan man trene?</h2>
+	<p>Vi har valgt å gå ut fra de sentrene som man finner i Oslo, ettersom vi retter oss mot studenter i Oslo. Under vil du da se hvilke
+		senter som man finner i Oslo, og hva de forskjellige sentrene tilbyr, hvor i Oslo de ligger og hvilke åpningstider de forskjellige 
+		har.</p>
 
 <h4>Elixia</h4>
+	<p>Gjennomsnittspris:</p>
+	<p>Binding: Tilbyr både med og uten binding. Tillegg om man velger uten binding.</p>
+	<p>Personlig trenere: Ja, i fire forskjellige nivåer</p>
+	<p>Gruppetimer: Ja, stort tilbud</p>
+	<p>Åpningstider:</p>
+	<p>Lokaler:</p>
+	<ul>
+		<li>Bjørvika</li>
+		<li>Carl Berner</li>
+		<li>Colosseum</li>
+		<li>Hoff</li>
+		<li>Lambertseter</li>
+		<li>Ringnes Park</li>
+		<li>Ryen</li>
+		<li>Sjølyst</li>
+		<li>Ullevål</li>
+	</ul>
+	<p>Elixia tilbyr også mange andre sentre i nærhet av Oslo, men vi valgte liste opp disse, da de er de mest sentrale.</p>
 	<a href="http://www.elixia.no">Elixia</a>
 
 <h4>SATS</h4>
+	<p>Gjennomsnittspris:</p>
+	<p>Binding:</p>
+	<p>Personlig trenere:</p>
+	<p>Gruppetimer:</p>
+	<p>Åpningstider:</p>
+	<p>Lokaler:</p>
+	<ul>
+		<li>Bislett</li>
+		<li>Ila</li>
+		<li>Nationalteateret</li>
+		<li>Njårdhallen</li>
+		<li>Nydalen</li>
+		<li>Sagene</li>
+		<li>Schous plass</li>
+		<li>Sjølyst</li>
+		<li>Solli plass</li>
+		<li>Spektrum</li>
+		<li>Vulkan</li>
+	</ul>
+	<p>SATS tilbyr også mange andre sentre i nærhet av Oslo, men vi valgte liste opp disse, da de er de mest sentrale.</p>
 	<a href="http://www.sats.no">SATS</a>
 
 <h4>EVO</h4>
+	<p>Gjennomsnittspris:</p>
+	<p>Binding:</p>
+	<p>Personlig trenere:</p>
+	<p>Gruppetimer:</p>
+	<p>Åpningstider:</p>
+	<p>Lokaler:</p>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
 	<a href="https://www.evofitness.no">EVO</a>
 
 <h4>Fitness 24seven</h4>
+	<p>Gjennomsnittspris:</p>
+	<p>Binding:</p>
+	<p>Personlig trenere:</p>
+	<p>Gruppetimer:</p>
+	<p>Åpningstider:</p>
+	<p>Lokaler:</p>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
 	<a href="http://www.fitness24seven.com/no/start">Fitness 24seven</a>
 
 <h4>Fresh fitness</h4>
+	<p>Gjennomsnittspris:</p>
+	<p>Binding:</p>
+	<p>Personlig trenere:</p>
+	<p>Gruppetimer:</p>
+	<p>Åpningstider:</p>
+	<p>Lokaler:</p>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
 	<a href="http://www.freshfitness.no">Fresh fitness</a>
 
 <h4>Fitness xpress</h4>
+	<p>Gjennomsnittspris:</p>
+	<p>Binding:</p>
+	<p>Personlig trenere:</p>
+	<p>Gruppetimer:</p>
+	<p>Åpningstider:</p>
+	<p>Lokaler:</p>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
 	<a href="http://www.fitnessxpress.no">Fitness xpress</a>
