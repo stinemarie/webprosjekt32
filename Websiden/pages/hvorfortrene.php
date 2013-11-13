8d5a356aadb3781498419e905d4ce01de13c7e36
amundring <amund@Amunds-MacBook-Pro-2.local>
2013-11-13 14:26:47 +0100

    "Hvorfor trene" CSS

--- Websiden/pages/hvorfortrene.php
+++ Websiden/pages/hvorfortrene.php
@@ -1,37 +1,24 @@
 <h2>Hvorfor trene?</h2>
 
-<p>En av de viktigste årsakene til å trene gjenvlig er at man får en økt livskvalitet og blir sterkere, som gjør at man blir
-	bedre rustet for de eldre årene.</p>
-<p>Trening vil også ikke bare bedre den fysiske delen, men også den psykiske delen og den indre delen av kroppen. Du får bedre 
-	humør, er energi og god selvfølelse ved litt fysisk aktivitet. Du reduserer også sjansene for ting som blodpropp, høyt blodtrykk, 
-	og gir en bedret lungekapasitet.</p>
+<div class="hvorfortrene">
+	<p>En av de viktigste årsakene til å trene gjenvlig er at man får en økt livskvalitet og blir sterkere, som gjør at man blir
+		bedre rustet for de eldre årene.</p>
+	<p>Trening vil også ikke bare bedre den fysiske delen, men også den psykiske delen og den indre delen av kroppen. Du får bedre 
+		humør, er energi og god selvfølelse ved litt fysisk aktivitet. Du reduserer også sjansene for ting som blodpropp, høyt blodtrykk, 
+		og gir en bedret lungekapasitet.</p>
 
-<p>Alle har vel også sett filmen "Rocky Balboa" fra 2006, eller kjenner til den kjente scenen, hvor han skal trene seg opp til den største
-og siste kampen, hvor han pusher seg selv for å nå målet, og klarer dette, som vises med en mestrende neve i været på toppen av trappen.</p>
-<figure>
-	<img src="./img/rockybalboa.jpg" alt="rocky balboa">
-		<figcaption class="kilde">kilde: IMDb.com </figcaption>
-</figure>
-<p>Her ser man noen som har nådd målet sitt og kjenner en mestringsfølelse. Er ikke dette noe du også vil kjenne?</p>
-<p>Ved trening kan du sette deg fine små mål over en kort periode, eller store mål over en lengre periode, og jobbe for å komme frem til dette.
-	Når det målet nåes, så vil du kjenne det samme som karakteren Rocky Balboa, og da kan du også slenge en neve i været! 
-	<strong>You did it!</strong></p>
+	<aside>
+		<p>Alle har vel også sett filmen "Rocky Balboa" fra 2006, eller kjenner til den kjente scenen, hvor han skal trene seg opp til den største
+		og siste kampen, hvor han pusher seg selv for å nå målet, og klarer dette, som vises med en mestrende neve i været på toppen av trappen.</p>
+		<p>Her ser man noen som har nådd målet sitt og kjenner en mestringsfølelse. Er ikke dette noe du også vil kjenne?</p>
+		<p>Ved trening kan du sette deg fine små mål over en kort periode, eller store mål over en lengre periode, og jobbe for å komme frem til dette.
+		Når det målet nåes, så vil du kjenne det samme som karakteren Rocky Balboa, og da kan du også slenge en neve i været! 
+		<strong>You did it!</strong></p>
+	</aside>
 
-<p class="kilde"> Kilde: <a href="www.imdb.com" target="_blank">IMDb.com </a></p>
+	<figure>
+		<img src="./img/rockybalboa.jpg" alt="rocky balboa">
+		<figcaption class="kilde">Kilde: <a href="www.imdb.com" target="_blank">IMDb.com </a></figcaption>
+	</figure>
 
-<h2> Body mass index kalkulator</h2>
-
-
-<p>BMI er en forkortelse for "Body mass index" og kan bli brukt til å bedømme kroppsvekt.
-BMI er et veldig grovt mål som ikke tar hensyn til faktorer som alder, kjønn eller muskelmass. Derfor kan en som er meget veltrent få en høyere bmi og bli klassifisert som overvektig. </p>
-
-<div>
-<form name="bmiKalk">
-	Din vekt i kilogram: <input id="vekt" type="text" name="vekt" size="10"><br />
-	Din høyde i centimeter: <input id="hoyde" type="text" name="høyde" size="10"><br />
-	<input type="button" id="button" value="Kalkuler BMI" onClick="kalkulerBmi()"><br />
-	Din BMI: <input id="visBMI" type="text" name="visBmi" size="10"><br />
-	Dette betyr: <input id="betydning" type="text" name="betydning" size="35"><br /><br /><br />
-	<input type="reset" value="Tilbakestill" />
-</form>
 </div>
