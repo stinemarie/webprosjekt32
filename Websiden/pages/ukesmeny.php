52134bf0c4bb6f0daa6c41e897871346a322a4de
amundring <amund@dhcp-158-39-197-124.wlan.hioa.no>
2013-11-14 11:17:32 +0100

    Ramme på dagene i ukesmenyen

--- Websiden/pages/ukesmeny.php
+++ Websiden/pages/ukesmeny.php
@@ -1,58 +1,74 @@
-<h2>Ukesmenyoversikt</h2>
-<p>Her er en oversikt over hvilke rett som hører til hvilke dag:</p>
+<div class="ukesmeny">	
+	<h2>Ukesmenyoversikt</h2>
+	<p>Her er en oversikt over hvilke rett som hører til hvilke dag:</p>
 
-<h4>MANDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Solskinn</p>
-	<p>Lunch:</p>
-		<p>Yoghurt med hjemmelaget granola</p>
-	<p>Middag:</p>
-		<p>Squashsalat med kylling, perlespelt og basilikumspesto</p>
+	<div class="dag">
+		<h4>MANDAG</h4>
+			<p>Frokost:</p>
+				<p>Smoothie - Solskinn</p>
+			<p>Lunch:</p>
+				<p>Yoghurt med hjemmelaget granola</p>
+			<p>Middag:</p>
+				<p>Squashsalat med kylling, perlespelt og basilikumspesto</p>
+	</div>
 
-<h4>TIRSDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Lyserød Drøm</p>
-	<p>Lunch:</p>
-		<p>Polarbrød med pålett</p>
-	<p>Middag:</p>
-		<p>Pasta med spicy tomatsaus og pecorino</p>
+	<div class="dag">
+		<h4>TIRSDAG</h4>
+			<p>Frokost:</p>
+				<p>Smoothie - Lyserød Drøm</p>
+			<p>Lunch:</p>
+				<p>Polarbrød med pålett</p>
+			<p>Middag:</p>
+				<p>Pasta med spicy tomatsaus og pecorino</p>
+	</div>
 
-<h4>ONSDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Salatsmoothie</p>
-	<p>Lunch:</p>
-		<p>Yoghurt med hjemmelaget granola</p>
-	<p>Middag:</p>
-		<p>Potet- og spinatsalat med laks</p>
+	<div class="dag">
+		<h4>ONSDAG</h4>
+			<p>Frokost:</p>
+				<p>Smoothie - Salatsmoothie</p>
+			<p>Lunch:</p>
+				<p>Yoghurt med hjemmelaget granola</p>
+			<p>Middag:</p>
+				<p>Potet- og spinatsalat med laks</p>
+	</div>
 
-<h4>TORSDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Ajopachi</p>
-	<p>Lunch:</p>
-		<p>Polarbrød med pålegg</p>
-	<p>Middag:</p>
-		<p>Tortellini med tomater og ruccula</p>
+	<div class="dag">
+		<h4>TORSDAG</h4>
+			<p>Frokost:</p>
+				<p>Smoothie - Ajopachi</p>
+			<p>Lunch:</p>
+				<p>Polarbrød med pålegg</p>
+			<p>Middag:</p>
+				<p>Tortellini med tomater og ruccula</p>
+	</div>
 
-<h4>FREDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Skippern</p>
-	<p>Lunch:</p>
-		<p>Nistepakke</p>
-	<p>Middag:</p>
-		<p>Hjemmelaget Pizza</p>
+	<div class="dag">
+		<h4>FREDAG</h4>
+			<p>Frokost:</p>
+				<p>Smoothie - Skippern</p>
+			<p>Lunch:</p>
+				<p>Nistepakke</p>
+			<p>Middag:</p>
+				<p>Hjemmelaget Pizza</p>
+	</div>
 
-<h4>LØRDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Dark Blue Heaven</p>
-	<p>Lunch:</p>
-		<p>Sommersalat med jordbær, eple og spekeskinke</p>
-	<p>Middag:</p>
-		<p>Sitruskylling med asparges og timian</p>
+	<div class="dag">
+		<h4>LØRDAG</h4>
+			<p>Frokost:</p>
+				<p>Smoothie - Dark Blue Heaven</p>
+			<p>Lunch:</p>
+				<p>Sommersalat med jordbær, eple og spekeskinke</p>
+			<p>Middag:</p>
+				<p>Sitruskylling med asparges og timian</p>
+	</div>
 
-<h4>SØNDAG</h4>
-	<p>Frokost:</p>
-		<p>Potetomelett med brokkoli og salami</p>
-	<p>Lunch:</p>
-		<p>Fougasse fylt med kylling, bacon, salat, rød pesto og aioli</p>
-	<p>Middag:</p>
-		<p>Spaghetti med spinat, soltørket tomat, hvitløkschips og feta</p>
+	<div class="dag">
+		<h4>SØNDAG</h4>
+			<p>Frokost:</p>
+				<p>Potetomelett med brokkoli og salami</p>
+			<p>Lunch:</p>
+				<p>Fougasse fylt med kylling, bacon, salat, rød pesto og aioli</p>
+			<p>Middag:</p>
+				<p>Spaghetti med spinat, soltørket tomat, hvitløkschips og feta</p>
+	</div>
+</div>
