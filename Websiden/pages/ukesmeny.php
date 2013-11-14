729befee0fcb9e10965ed2b0e2425a1f36787f85
idaings <IdaVikaskagSalvesen@dhcp-158-39-193-244.wlan.hioa.no>
2013-11-14 10:35:27 +0100

    ukesmeny.php

--- Websiden/pages/ukesmeny.php
+++ Websiden/pages/ukesmeny.php
@@ -1,3 +1,72 @@
-<div class="ukesmeny">
-	<h2>Ukesmeny</h2>
-</div>
+<h2>Ukesmenyoversikt</h2>
+<p>Her er en oversikt over hvilke rett som hører til hvilke dag:</p>
+
+<h4>MANDAG</h4>
+	<p>Frokost:</p>
+		<p>Smoothie - Solskinn</p>
+	<p>Lunch:</p>
+		<p></p>
+	<p>Middag:</p>
+		<p>Squashsalat med kylling, perlespelt og basilikumspesto</p>
+	<p>Nattsnack:</p>
+		<p></p>
+
+<h4>TIRSDAG</h4>
+	<p>Frokost:</p>
+		<p>Smoothie - Lyserød Drøm</p>
+	<p>Lunch:</p>
+		<p></p>
+	<p>Middag:</p>
+		<p>Pasta med spicy tomatsaus og pecorino</p>
+	<p>Nattsnack:</p>
+		<p></p>
+
+<h4>ONSDAG</h4>
+	<p>Frokost:</p>
+		<p>Smoothie - Salatsmoothie</p>
+	<p>Lunch:</p>
+		<p></p>
+	<p>Middag:</p>
+		<p>Potet- og spinatsalat med laks</p>
+	<p>Nattsnack:</p>
+		<p></p>
+
+<h4>TORSDAG</h4>
+	<p>Frokost:</p>
+		<p>Smoothie - Ajopachi</p>
+	<p>Lunch:</p>
+		<p></p>
+	<p>Middag:</p>
+		<p>Tortellini med tomater og ruccula</p>
+	<p>Nattsnack:</p>
+		<p></p>
+
+<h4>FREDAG</h4>
+	<p>Frokost:</p>
+		<p>Smoothie - Skippern</p>
+	<p>Lunch:</p>
+		<p></p>
+	<p>Middag:</p>
+		<p>Hjemmelaget Pizza</p>
+	<p>Nattsnack:</p>
+		<p></p>
+
+<h4>LØRDAG</h4>
+	<p>Frokost:</p>
+		<p>Smoothie - Dark Blue Heaven</p>
+	<p>Lunch:</p>
+		<p>Sommersalat med jordbær, eple og spekeskinke</p>
+	<p>Middag:</p>
+		<p>Sitruskylling med asparges og timian</p>
+	<p>Nattsnack:</p>
+		<p></p>
+
+<h4>SØNDAG</h4>
+	<p>Frokost:</p>
+		<p>Omelett/Stekt egg med bacon og salat</p>
+	<p>Lunch:</p>
+		<p></p>
+	<p>Middag:</p>
+		<p>Spaghetti med spinat, soltørket tomat, hvitløkschips og feta</p>
+	<p>Nattsnack:</p>
+		<p></p>
