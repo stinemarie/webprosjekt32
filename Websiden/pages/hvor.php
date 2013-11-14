ac9ced8fa7d3868d4af31cf31b80b2f744455036
amundring <amund@dhcp-158-39-197-124.wlan.hioa.no>
2013-11-14 11:09:34 +0100

    Litt CSS på "trene hvor", mangler litt innhold

--- Websiden/pages/hvor.php
+++ Websiden/pages/hvor.php
@@ -1,263 +1,276 @@
-<h2>Hvor kan man trene?</h2>
-	<p>Vi har valgt å gå ut fra de sentrene som man finner i Oslo, ettersom vi retter oss mot studenter i Oslo. Under vil du da se hvilke
-		senter som man finner i Oslo, og hva de forskjellige sentrene tilbyr, hvor i Oslo de ligger og hvilke åpningstider de forskjellige 
-		har.</p>
+<div class="hvor">	
 
-<h4>Elixia</h4>
-	Gjennomsnitspris:
-	<p>Binding: Tilbyr både med og uten binding. Tillegg om man velger uten binding.</p>
-	<p>Personlig trenere: Ja, i fire forskjellige nivåer</p>
-	<p>Gruppetimer: Ja, stort tilbud</p>
-	<p>Åpningstider:</p>
-	<p>Lokaler:</p>
-	<ul>
-		<li>Bjørvika</li>
-		<li>Carl Berner</li>
-		<li>Colosseum</li>
-		<li>Hoff</li>
-		<li>Lambertseter</li>
-		<li>Ringnes Park</li>
-		<li>Ryen</li>
-		<li>Sjølyst</li>
-		<li>Ullevål</li>
-	</ul>
-	<p>Elixia tilbyr også mange andre sentre i nærhet av Oslo, men vi valgte liste opp disse, da de er de mest sentrale.</p>
-	<a href="http://www.elixia.no">Elixia</a><br />
+	<h2>Hvor kan man trene?</h2>
+		<p>Vi har valgt å gå ut fra de sentrene som man finner i Oslo, ettersom vi retter oss mot studenter i Oslo. Under vil du da se hvilke
+			senter som man finner i Oslo, og hva de forskjellige sentrene tilbyr, hvor i Oslo de ligger og hvilke åpningstider de forskjellige 
+			har.</p>
 
-<a href="https://www.sats.no" target="_blank">
-<img border="0" src="img/satslogo.png" alt="Sats" width="100" height="50"></a>
-
-	<div class="treninghvor">
-	<table>
-	<thead>
-	<th>Medlemskap</th>
-	<th>Ingen bindingstid</th>
-	<th>12 måneders binding</th>
-	<th>12 måneders forhåndsbetalt</th>
-	</thead>
-	<tr class="alt">
-	<td>Sats Club - Double</td>
-	<td>508,-/mnd</td>
-	<td>460,-/mnd</td>
-	<td>5520;-</td>
-	</tr>
-	<tr class="alt">
-	<td>Sats Club City</td>
-	<td>524,-/mnd</td>
-	<td>476,-/mnd</td>
-	<td>5712,-</td>
-	</tr>
-	<tr class="alt">
-	<td>Sats Club Country</td>
-	<td>556,-/mnd</td>
-	<td>508,-/mnd</td>
-	<td>6096,-</td>
-	</tr>
-	</table>
-	</div>
-
-	<p>Personlig trenere: Ja</p> 
-	<p>Gruppetimer: Ja</p>
-	<p>Åpningstider: 
-	<ul>
-	<li>Mandag - Torsdag: 06.00-23.00</li>
-	<li>Fredag: 06.00-21.00.</li>
-	<li>Lørdag: 08.00-18.00</li>
-	<li>Søndag: 09.00-20.00</li>
-	</ul>
-	</p>
-	<p>Lokaler:</p>
-	<ul>
-		<li>Bislett</li>
-		<li>Ila</li>
-		<li>Nationalteateret</li>
-		<li>Njårdhallen</li>
-		<li>Nydalen</li>
-		<li>Sagene</li>
-		<li>Schous plass</li>
-		<li>Sjølyst</li>
-		<li>Solli plass</li>
-		<li>Spektrum</li>
-		<li>Vulkan</li>
-	</ul>
-	<p>SATS tilbyr også mange andre sentre i nærhet av Oslo, men vi valgte liste opp disse, da de er de mest sentrale.</p>
-	
-
-<a href="https://www.evofitness.no" target="_blank">
-<img border="0" src="img/evofitness.png" alt="EVO Fitness" width="100" height="50"></a>
-
-	<div class="treninghvor">
-	<table>
-	<thead>
-	<th>Medlemskap</th>
-	<th>Ingen bindingstid</th>
-	<th>12 måneders forhåndsbetalt</th>
-	</thead>
-	<tr class="alt">
-	<td>Alle sentere</td>
-	<td>295,-/mnd</td>
-	<td>3540,-</td>
-	</table>
+	<div class="treningsenter">
+		<h4>Elixia</h4>
+			Gjennomsnitspris:
+			<p>Binding: Tilbyr både med og uten binding. Tillegg om man velger uten binding.</p>
+			<p>Personlig trenere: Ja, i fire forskjellige nivåer</p>
+			<p>Gruppetimer: Ja, stort tilbud</p>
+			<p>Åpningstider:</p>
+			<p>Lokaler:</p>
+			<ul>
+				<li>Bjørvika</li>
+				<li>Carl Berner</li>
+				<li>Colosseum</li>
+				<li>Hoff</li>
+				<li>Lambertseter</li>
+				<li>Ringnes Park</li>
+				<li>Ryen</li>
+				<li>Sjølyst</li>
+				<li>Ullevål</li>
+			</ul>
+			<p>Elixia tilbyr også mange andre sentre i nærhet av Oslo, men vi valgte liste opp disse, da de er de mest sentrale.</p>
+			<a href="http://www.elixia.no">Elixia</a>
 	</div>
 
 
-	<p>Gjennomsnittspris:</p>
-	<p>Binding:</p>
-	<p>Personlig trenere:</p>
-	<p>Gruppetimer:</p>
-	<p>Åpningstider:</p>
-	<p>Lokaler:</p>
-	<ul>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
+	<div class="treningsenter">
+		<a href="https://www.sats.no" target="_blank">
+		<img border="0" src="img/satslogo.png" alt="Sats" width="100" height="50"></a>
 
-<a href="https://www.fitness24seven.com/no/start" target="_blank">
-<img border="0" src="img/fitness247.png" alt="Fitness 24/7" width="50" height="50"></a>
-	
-	<div class="treninghvor">
-	<table>
-	<thead>
-	<th>Medlemskap</th>
-	<th>Hele døgnet</th>
-	<th>Dagskort</th>
-	<th>Forhåndbetalt "Hele døgnet"</th>
-	<th>Forhåndsbetalt "Dagskort"</th>
-	</thead>
-	<tr class="alt">
-	<td>Alle sentere</td>
-	<td>199,-/mnd</td>
-	<td>99,-/mnd</td>
-	<td>2695,-</td>
-	<td>1495,-</td>
-	</table>
+			<div class="treninghvor">
+			<table>
+			<thead>
+			<th>Medlemskap</th>
+			<th>Ingen bindingstid</th>
+			<th>12 måneders binding</th>
+			<th>12 måneders forhåndsbetalt</th>
+			</thead>
+			<tr class="alt">
+			<td>Sats Club - Double</td>
+			<td>508,-/mnd</td>
+			<td>460,-/mnd</td>
+			<td>5520;-</td>
+			</tr>
+			<tr class="alt">
+			<td>Sats Club City</td>
+			<td>524,-/mnd</td>
+			<td>476,-/mnd</td>
+			<td>5712,-</td>
+			</tr>
+			<tr class="alt">
+			<td>Sats Club Country</td>
+			<td>556,-/mnd</td>
+			<td>508,-/mnd</td>
+			<td>6096,-</td>
+			</tr>
+			</table>
+			</div>
+
+			<p>Personlig trenere: Ja</p> 
+			<p>Gruppetimer: Ja</p>
+			<p>Åpningstider: 
+			<ul>
+			<li>Mandag - Torsdag: 06.00-23.00</li>
+			<li>Fredag: 06.00-21.00.</li>
+			<li>Lørdag: 08.00-18.00</li>
+			<li>Søndag: 09.00-20.00</li>
+			</ul>
+			</p>
+			<p>Lokaler:</p>
+			<ul>
+				<li>Bislett</li>
+				<li>Ila</li>
+				<li>Nationalteateret</li>
+				<li>Njårdhallen</li>
+				<li>Nydalen</li>
+				<li>Sagene</li>
+				<li>Schous plass</li>
+				<li>Sjølyst</li>
+				<li>Solli plass</li>
+				<li>Spektrum</li>
+				<li>Vulkan</li>
+			</ul>
+			<p>SATS tilbyr også mange andre sentre i nærhet av Oslo, men vi valgte liste opp disse, da de er de mest sentrale.</p>
+	</div>		
+
+	<div class="treningsenter">
+		<a href="https://www.evofitness.no" target="_blank">
+		<img border="0" src="img/evofitness.png" alt="EVO Fitness" width="100" height="50"></a>
+
+			<div class="treninghvor">
+			<table>
+			<thead>
+			<th>Medlemskap</th>
+			<th>Ingen bindingstid</th>
+			<th>12 måneders forhåndsbetalt</th>
+			</thead>
+			<tr class="alt">
+			<td>Alle sentere</td>
+			<td>295,-/mnd</td>
+			<td>3540,-</td>
+			</table>
+			</div>
+
+
+			<p>Gjennomsnittspris:</p>
+			<p>Binding:</p>
+			<p>Personlig trenere:</p>
+			<p>Gruppetimer:</p>
+			<p>Åpningstider:</p>
+			<p>Lokaler:</p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
 	</div>
 
+	<div class="treningsenter">
+		<a href="https://www.fitness24seven.com/no/start" target="_blank">
+		<img border="0" src="img/fitness247.png" alt="Fitness 24/7" width="50" height="50"></a>
+			
+			<div class="treninghvor">
+			<table>
+			<thead>
+			<th>Medlemskap</th>
+			<th>Hele døgnet</th>
+			<th>Dagskort</th>
+			<th>Forhåndbetalt "Hele døgnet"</th>
+			<th>Forhåndsbetalt "Dagskort"</th>
+			</thead>
+			<tr class="alt">
+			<td>Alle sentere</td>
+			<td>199,-/mnd</td>
+			<td>99,-/mnd</td>
+			<td>2695,-</td>
+			<td>1495,-</td>
+			</table>
+			</div>
 
-	<p>Gjennomsnittspris:</p>
-	<p>Binding:</p>
-	<p>Personlig trenere:</p>
-	<p>Gruppetimer:</p>
-	<p>Åpningstider:</p>
-	<p>Lokaler:</p>
-	<ul>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
 
-<h4>Fresh fitness</h4>
-	<p>Gjennomsnittspris:</p>
-	<p>Binding:</p>
-	<p>Personlig trenere:</p>
-	<p>Gruppetimer:</p>
-	<p>Åpningstider:</p>
-	<p>Lokaler:</p>
-	<ul>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-	<a href="http://www.freshfitness.no">Fresh fitness</a>
+			<p>Gjennomsnittspris:</p>
+			<p>Binding:</p>
+			<p>Personlig trenere:</p>
+			<p>Gruppetimer:</p>
+			<p>Åpningstider:</p>
+			<p>Lokaler:</p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
 
-<h4>Fitness xpress</h4>
-	<p>Gjennomsnittspris:</p>
-	<p>Binding:</p>
-	<p>Personlig trenere:</p>
-	<p>Gruppetimer:</p>
-	<p>Åpningstider:</p>
-	<p>Lokaler:</p>
-	<ul>
-		<li></li>
-		<li></li>
-		<li></li>
-	</ul>
-	<a href="http://www.fitnessxpress.no">Fitness xpress</a>
+		<h4>Fresh fitness</h4>
+			<p>Gjennomsnittspris:</p>
+			<p>Binding:</p>
+			<p>Personlig trenere:</p>
+			<p>Gruppetimer:</p>
+			<p>Åpningstider:</p>
+			<p>Lokaler:</p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<a href="http://www.freshfitness.no">Fresh fitness</a>
+	</div>
 
-<p> Når det kommer til de forskjellige treningssenterene som ligger plassert rundt om kring er det et stort sprik når det kommer til pris og tilbud. 
-Du vil under her få en liste med forskjellige priser som er beregnet for studenter.</p>
+	<div class="treningsenter">
+		<h4>Fitness xpress</h4>
+			<p>Gjennomsnittspris:</p>
+			<p>Binding:</p>
+			<p>Personlig trenere:</p>
+			<p>Gruppetimer:</p>
+			<p>Åpningstider:</p>
+			<p>Lokaler:</p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<a href="http://www.fitnessxpress.no">Fitness xpress</a>
 
-<h3><a href="http://www.sio.no/wps/portal/!ut/p/c5/04_SB8K8xLLM9MSSzPy8xBz9CP0os3gDfwNvJ0dTP0NXAyNDA38TS3cDKADKR2LKmyDkidGNAzgS0h2cl68fDnIxfheA5XHb4eeRn5uqX5AbGlHhqesIAGC10lM!/dl3/d3/L2dJQSEvUUt3QS9ZQnZ3LzZfME8wS0JBNU4xRTAyMTBPNDlHMDAwMDAwMDA!/?reset=true">Athletica</a></h3>
-<div class="treninghvor">
-<table>
-<thead>
-<th>Medlemskap</th>
-<th> Semester</th>
-<th>Per Måned</th>
-<th> Semester (Man-Fre frem itil kl. 13.)</th>
-<th>Per måned</th>
-<th>Enkelbilett/Klippekort</th>
-</tr>
-</thead>
-<tr class="alt">
-<td>Student UiO, HiOA, NVH, AHO, NMH, KhIO</td>
-<td>850,-</td>
-<td>155,-</td>
-<td>723,-</td>
-<td>132,-</td>
-<td>120,-/990,-</td>
-</tr>
-<tr class="alt">
-<td>Student BI, AMH, WH</td>
-<td>1350,-</td>
-<td>255,-</td>
-<td>1148,-</td>
-<td>209,-</td>
-<td>120,-/990,-</td>
-</tr>
-<tr class="alt">
-<td>Student andre SiO studenter (se sio.no)</td>
-<td>1800,-</td>
-<td>327,-</td>
-<td>1530,-</td>
-<td>278,-</td>
-<td>120,-/990,-</td>
-</tr>
-<tr class="alt">
-<td>Student uten betalt semesteravgift til SiO</td>
-<td>1900,-</td>
-<td>345,-</td>
-<td>1615,-</td>
-<td>293,-</td>
-<td>120,-/990,-</td>
-</tr>
-<tr class="alt">
-<td>Student Diakonhjemmet</td>
-<td>1200,-</td>
-<td>218,-</td>
-<td>n/a</td>
-<td>n/a</td>
-<td>120,-/990,-</td>
-</tr>
-<tr class="alt">
-<td>Ikke tilknyttet SIO eller student</td>
-<td>2650,-</td>
-<td>482,-</td>
-<td>n/a</td>
-<td>n/a</td>
-<td>120,-/990,-</td>
-</tr>
-</table>
+		<p> Når det kommer til de forskjellige treningssenterene som ligger plassert rundt om kring er det et stort sprik når det kommer til pris og tilbud. 
+		Du vil under her få en liste med forskjellige priser som er beregnet for studenter.</p>
+	</div>
+
+	<div class="treningsenter">
+		<h3><a href="http://www.sio.no/trening">Athletica</a></h3>
+		<div class="treninghvor">
+		<table>
+		<thead>
+		<th>Medlemskap</th>
+		<th> Semester</th>
+		<th>Per Måned</th>
+		<th> Semester (Man-Fre frem itil kl. 13.)</th>
+		<th>Per måned</th>
+		<th>Enkelbilett/Klippekort</th>
+		</tr>
+		</thead>
+		<tr class="alt">
+		<td>Student UiO, HiOA, NVH, AHO, NMH, KhIO</td>
+		<td>850,-</td>
+		<td>155,-</td>
+		<td>723,-</td>
+		<td>132,-</td>
+		<td>120,-/990,-</td>
+		</tr>
+		<tr class="alt">
+		<td>Student BI, AMH, WH</td>
+		<td>1350,-</td>
+		<td>255,-</td>
+		<td>1148,-</td>
+		<td>209,-</td>
+		<td>120,-/990,-</td>
+		</tr>
+		<tr class="alt">
+		<td>Student andre SiO studenter (se sio.no)</td>
+		<td>1800,-</td>
+		<td>327,-</td>
+		<td>1530,-</td>
+		<td>278,-</td>
+		<td>120,-/990,-</td>
+		</tr>
+		<tr class="alt">
+		<td>Student uten betalt semesteravgift til SiO</td>
+		<td>1900,-</td>
+		<td>345,-</td>
+		<td>1615,-</td>
+		<td>293,-</td>
+		<td>120,-/990,-</td>
+		</tr>
+		<tr class="alt">
+		<td>Student Diakonhjemmet</td>
+		<td>1200,-</td>
+		<td>218,-</td>
+		<td>n/a</td>
+		<td>n/a</td>
+		<td>120,-/990,-</td>
+		</tr>
+		<tr class="alt">
+		<td>Ikke tilknyttet SIO eller student</td>
+		<td>2650,-</td>
+		<td>482,-</td>
+		<td>n/a</td>
+		<td>n/a</td>
+		<td>120,-/990,-</td>
+		</tr>
+		</table>
+		</div>
+	</div>
+
+	<div class="treningsenter">
+		<h3><a href="www.fitnessxpress.no">Fitness Xpress</a></h3>
+		<div class="treninghvor">
+		<table>
+		<thead>
+		<th>Medlemskap</th>
+		<th>Ingen bindingstid</th>
+		<th>12 måneders forhåndsbetalt</th>
+		</thead>
+		<tr class="alt">
+		<td>Alle sentere</td>
+		<td>199,-/mnd</td>
+		<td>2388,-</td>
+		</table>
+		</div>
+		<p>Fresh Xpress tilbyr ingen rabatter til studenter. De har derimot et fast pris på 199,-/mnd uten binding. Dette gir deg tilgang til alle senterene de har tilgjengelig.</p>
+	</div>
+
 </div>
-
-
-<h3><a href="www fitnessxpress no">Fitness Xpress</a></h3>
-<div class="treninghvor">
-<table>
-<thead>
-<th>Medlemskap</th>
-<th>Ingen bindingstid</th>
-<th>12 måneders forhåndsbetalt</th>
-</thead>
-<tr class="alt">
-<td>Alle sentere</td>
-<td>199,-/mnd</td>
-<td>2388,-</td>
-</table>
-</div>
-<p>Fresh Xpress tilbyr ingen rabatter til studenter. De har derimot et fast pris på 199,-/mnd uten binding. Dette gir deg tilgang til alle senterene de har tilgjengelig.</p>
-
-
-
-<!-- Vet ikke om jeg skal ha med flere treningsentere, da jeg føler at det blir mye av det samme bare forskjellige navn-->
