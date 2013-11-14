a9579fbebccce9e20f833d10774ced01865fc0b7
idaings <IdaVikaskagSalvesen@dhcp-158-39-193-244.wlan.hioa.no>
2013-11-14 11:37:22 +0100

    Ernæring svar fra Maiken Walle

--- Websiden/pages/ernering.php
+++ Websiden/pages/ernering.php
@@ -1,3 +1,59 @@
 <div class="ernæring">
 	<h2>Ernæring</h2>
+	<p>Vi har fått Maiken Walle, som er ernæringsstudent på siste året til å svare på noen spørsmål knyttet til kosthold og trening.</p>
+
+	<p><b>Hva burde en hver middag innehold for å få nok energi og næring fra måltidet?</b></p>
+	<p>Enhver middag burde legges opp etter tallerkenmoddelen, hvor man har:</p> 
+	<ul>
+		<li>1/3 Karbohydrater, (som potet, brød, pasta - Grove varianter)</li>
+		<li>1/3 Proteiner, (som kjøtt, fisk, linser, bønner)</li>
+		<li>1/3 Grønnsaker</li>
+	</ul>
+	<p>Ved ønske om vektnedgang, så endres det noe, da vil modellen se slik ut:</p>
+	<ul>
+		<li>1/4 Karbohydrater</li>
+		<li>1/4 Proteiner</li>
+		<li>2/4 Grønnsaker</li>
+	</ul>
+	<p>Det er nok mange som vil si seg enig i dette, da man kanskje følger lavkarbo dietter, er vegitarianere el. men hovedfokuset i å 
+		sette det opp slik, er for å vise at det er SAMMENSETNINGEN av et måltid som burde være i fokus, at det burde inneholde alle
+		matgruppene våres.</p>
+	
+	<p><b>Er det noen enkle tips til studenter for å få et innholdsrikt måltid innenfor et budsjett? Med tanke på hva slags produkter 
+		er billig med mye næring som kan brukes i flere måltider</b></p>
+	<p>Egg! Egg inneholder faktisk alle vitaminer og mineraler, med unntak av Vitamin C. Så en mer sammensatt matvare skal man lete lenge
+		etter. Dessuten så metter egg godt, og har svært mange bruksområder.</p>
+	<p>Så er det sånn at mange tror det skal være dyrt å spise sunt, men det stemmer ikke! Grønnsaker er ikke dyrt, og spesielt er
+		rotgrønnsaker flotte og veldig billige. Dette kan brukes til å lage rotmos, ovnsbakt tilbehør, og som snack (kålrot, gulrot og dipp - NAM).
+		Fisk er også noe som studenter burde bli flinkere til å kjøpe, og trenger ikke være dyrt. Om man synes det er vanskelig å finne på hva 
+		man kan gjøre med fisken - kan man ha feks. laks i wrap sammen med ruccula og philadelphia ost, man kan ha wok med torsk/laks/annen fisk, og
+		da kan man ha restene med i matboksen på skolen dagen etter.</p>
+
+	<p><b>Hva burde man spise før/etter trening?</b></p>
+	<p>Dette er et veldig omdiskutert tema, og dessuten veldig vanskelig å svare på. Grunnen til dette er at svaret avhenger veldig av hva
+		slags person det gjelder. Er du feks. en toppiddrettsutøver er det veldig viktig å ha opparbeidet gode glukogenlagre (fra å ha spist
+		masse karbohydrater) i tiden før trening. Det er også viktig for dem å innta karbohydrater og protein direkte etter trening. Årsaken
+		til dette er for å bedre restitusjonen, som nettopp er den store tingen her...Kroppen bruker ca 8 timer på å gjenoppbygge glukogenslagrene
+		sine etter en treningsøkt (forutsetter at man har drevet med en type trening, hvor disse tømmes, som Sykling, Cardio, Løping).</p>
+	<p>For en vanlig person, som kun trener en gang om dagen, eller mindre, vil kroppen uansett restituere seg innen neste økt. Det er dermed 
+		ikke noe behov for å få i seg en energybar eller energydrikk rett etter trening. Dette vil i all hovedsak ikke gjøre noe med kroppen, 
+		annet enn å gjøre at man spiser enda fler kcal rett etter trening, enn det du har forbrendt. Dessuten inneholder disse barene og 
+		drikkene rask energi (les: sukker), derav navnet Energybar. Men jeg vil si at man burde spise innen 1 time etter trening, som bør
+		være et sammensatt måltid. Evt om det vil gå lengre tid før man får til dette, så kan man spise en banan (karb), en yoghurt (karb og prot)
+		eller en brødskive med et glass melk.</p>
+	<p>Jeg kan da med andre ord si at jeg er veldig imot disse energy-produktene.</p>
+	<p>Kort oppsummert for en vanlig person: Spis et måltid et par timer før, gjerne karbohydratrikt, hvis du skal drive med utholdenhetstrening,
+		og husk å drikke godt underveis for å opprettholde væskebalansen. Så må man spise et sammensatt måltid ca 1 time etter og drikke masse
+		væske.</p>
+
+	<p><b>Hva mener du er det viktigste å få i seg hver dag? Mat/drikke?</b></p>
+
+	<p><b>Fisk eller kjøtt?</b></p>
+
+	<p><b>Vegetar eller ikke?</b></p>
+
+	<p><b>Typiske feller? à typisk som folk faller for (trener for mye? Dietter?)</b></p>
+
+	<p><b>Hva skal man styre unna? Sundt fett/ikke sundt fett?</b></p>
+
 </div>
