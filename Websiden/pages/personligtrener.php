fad15d7dcd05d549f5de38196fee38f56b0d9a58
amundring <amund@dhcp-158-39-197-124.wlan.hioa.no>
2013-11-14 11:54:39 +0100

    Litt CSS på "Personlig Trener"

--- Websiden/pages/personligtrener.php
+++ Websiden/pages/personligtrener.php
@@ -11,137 +11,165 @@
 
 	<!-- Må sette inn bilder og linke til nettsiden til de forkjsellige PT'ene, også endre størresle til å se bra ut. -->
 
-	<a href="https://www.facebook.com/martin.antonio.5648" target="_blank">
-	<img border="0" src="img/martin.png" alt="Martin Antonio" width="100" height="100"></a>
-	<a href="http://www.optimaltrening.com/adrian-paul/" target="_blank">
-	<img border="0" src="img/adrian.jpg" alt="Adrian Paul" width="100" height="100"></a>
-	<a href="http://prestasjonstrener.com/tomas-fjeldberg/" target="_blank">
-	<img border="0" src="img/tomas.jpg" alt="Tomas Fjeldberg" width="100" height="100"></a>
+	<div id="trenerne">
+		<figure>
+			<a href="https://www.facebook.com/martin.antonio.5648" target="_blank">
+				<img border="0" src="img/martin.jpeg" alt="Martin Antonio">
+			</a>
+			<figcaption>Martin Antonio</figcaption>
+		</figure>
+		<figure>
+			<a href="http://www.optimaltrening.com/adrian-paul/" target="_blank">
+				<img border="0" src="img/adrian.jpg" alt="Adrian Paul">
+			</a>
+			<figcaption>Adrian Paul</figcaption>
+		</figure>
+		<figure>
+			<a href="http://prestasjonstrener.com/tomas-fjeldberg/"target="_blank">
+				<img border="0" src="img/tomas.jpg" alt="Tomas Fjeldberg">
+			</a>
+			<figcaption>Tomas Fjeldberg</figcaption>
+		</figure>
+	</div>
 
-	<p><b> Hva er din treningsfilosofi?</b></p>
 
-	<!--Endre bilder og tekst -->
+	<div class="question">
+		<p><b> Hva er din treningsfilosofi?</b></p>
 
-	<ul>
+		<!--Endre bilder og tekst -->
+
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a> :   Uansett hva som skal til, uavhengig av tid, penger, venner og bekjente, så er det dette jeg skal gjøre og vil gjøre resten av mitt liv. Jeg elsker å føle meg nedbrutt etter en god treningsøkt.
+			 Treningssmerte er noe av det beste jeg vet.</li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a> :   Tren styrke, det holder deg sterk psykisk 
+				og mentalt resten av livet!</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a> :   Ha kontinuitet på det du gjør og gjør det enkelt.</li>
+		</ul>
+	</div>
+
+	<div class="question">
+		<p><b> Hva er det som motiverer deg, og har du noen tips til å finne motivasjon på tunge dager?</b></p>
+
+		<!--Endre bilder og tekst -->
+
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  Det som motiverer meg med trening er mestringsfølelsen og alle de gode helseaspektene ved trening. Jeg føler med bedre, sover bedre og er så å si alltid i godt humør. 
+			Jeg elsker å utfordre meg selv og er alltid ute etter å pushe grenser og sette nye rekorder. Det er først og fremst meg selv jeg ønsker å slå og er i en konkurranse mot meg selv, ikke andre. <br />
+			Du må ha et sterkt hvorfor: Mitt sterkeste hvorfor er helse og trening. Jeg ønsker å inspirere andre samt hjelpe andre som har slitt med skader, helseproblemer og annet.
+			Jeg har hatt en del problemer med helsa/kroppen grunnet sykdom, og vet hvordan det er å være i gjørma. 
+			Derfor ønsker jeg å gjøre mest mulig for andre ved å vise at det er mulig uansett hva som skjer, samt vise vei og være et forbilde.</li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Har man satt seg realistiske mål så er det 
+				aldri vanskelig å motivere seg!</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Målet mitt. Ikke fokuser på veien til
+				Mål, men lev deg inn i øyeblikket der du er i mål og kjenn på følelsen det vil gi deg og hva det bringer med 
+				seg av positive konsekvenser.</li>
+		</ul>
+	</div>
+
+
+	<div class="question">
+		<p><b> Hva er det viktig å ha høy fokus på ved trening?</b></p>
+
+		<!--Endre bilder og tekst -->
+
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  Det er 3 ting jeg mener er utrolig viktig: kosthold, riktig teknisk utførelse av alle øvelser og nok søvn. 
+			Andre ting er treningsstruktur og at du følger ett opplegg over lengre tid. Det tar tid å få resultater og kontinuitet er det viktigste på de ulike områdene.</li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Kvalitet fremfor kvantitet, holde seg 
+				skadefri ved å trene teknisk korrekt</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Kvalitet forran kvantitet.</li>
+							
+		</ul>
+	</div>
+
+
+	<div class="question">
+		<p><b> Hvor mange økter burde man ha per uke for å se resultater?</b></p>
+
+		<!--Endre bilder og tekst -->
+
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  Jeg anbelaler minimum 3 økter i uken der du går gjennom hele kroppen. </li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Det kommer an på målet ditt.</li>
+			<li><img src="img/tomas.png" alt="Tomas Fjeldberg" width="32" height="32"></a>  Tre.</li>
 						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a> :   Uansett hva som skal til, uavhengig av tid, penger, venner og bekjente, så er det dette jeg skal gjøre og vil gjøre resten av mitt liv. Jeg elsker å føle meg nedbrutt etter en god treningsøkt.
-		 Treningssmerte er noe av det beste jeg vet.</li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a> :   Tren styrke, det holder deg sterk psykisk 
-			og mentalt resten av livet!</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a> :   Ha kontinuitet på det du gjør og gjør det enkelt.</li>
-	</ul>
+		</ul>
+	</div>
 
-	<p><b> Hva er det som motiverer deg, og har du noen tips til å finne motivasjon på tunge dager?</b></p>
+	<div class="question">
+		<p><b> Hva er dine fem favoritt-øvelser?</b></p>
 
-	<!--Endre bilder og tekst -->
+		<!--Endre bilder og tekst -->
 
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  Det som motiverer meg med trening er mestringsfølelsen og alle de gode helseaspektene ved trening. Jeg føler med bedre, sover bedre og er så å si alltid i godt humør. 
-		Jeg elsker å utfordre meg selv og er alltid ute etter å pushe grenser og sette nye rekorder. Det er først og fremst meg selv jeg ønsker å slå og er i en konkurranse mot meg selv, ikke andre. <br />
-		Du må ha et sterkt hvorfor: Mitt sterkeste hvorfor er helse og trening. Jeg ønsker å inspirere andre samt hjelpe andre som har slitt med skader, helseproblemer og annet.
-		Jeg har hatt en del problemer med helsa/kroppen grunnet sykdom, og vet hvordan det er å være i gjørma. 
-		Derfor ønsker jeg å gjøre mest mulig for andre ved å vise at det er mulig uansett hva som skjer, samt vise vei og være et forbilde.</li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Har man satt seg realistiske mål så er det 
-			aldri vanskelig å motivere seg!</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Målet mitt. Ikke fokuser på veien til
-			Mål, men lev deg inn i øyeblikket der du er i mål og kjenn på følelsen det vil gi deg og hva det bringer med 
-			seg av positive konsekvenser.</li>
-	</ul>
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  Muscle Up, Knebøy, Markløft, Pullups og Benkpress</li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Markløft, knebøy, benkpress, muscle ups, 
+				front lever</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Støt, knebøy, rykk, markløft og benkpress.</li>
+			
+		</ul>
+	</div>
 
+	<div class="question">
+		<p><b> Hva slags trening uttenfor treningssenter vil du anbefale?</b></p>
 
-	<p><b> Hva er det viktig å ha høy fokus på ved trening?</b></p>
+		<!--Endre bilder og tekst -->
 
-	<!--Endre bilder og tekst -->
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  Det viktigste er at du får i deg nok mat og har et godt grunnlag. 
+			En gåtur i skogen er fin trening. Du kan også trene på 40, 60 og 100 meter sprint. Det er også mulig å trene i barnehageparker og andre arenaer kun ved bruk av egen kroppsvekt. </li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Calisthenics (kroppsvektstrening).</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Ski, svømming, fjellturer, 
+				ballidretter hva som helst egentlig. </li>
 
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  Det er 3 ting jeg mener er utrolig viktig: kosthold, riktig teknisk utførelse av alle øvelser og nok søvn. 
-		Andre ting er treningsstruktur og at du følger ett opplegg over lengre tid. Det tar tid å få resultater og kontinuitet er det viktigste på de ulike områdene.</li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Kvalitet fremfor kvantitet, holde seg 
-			skadefri ved å trene teknisk korrekt</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Kvalitet forran kvantitet.</li>
-						
-	</ul>
+		</ul>
+	</div>
 
+	<div class="question">
+		<p><b>Hvis du skal anbefale kosttilskudd, hva vil det være?</b></p>
 
-	<p><b> Hvor mange økter burde man ha per uke for å se resultater?</b></p>
+		<!--Endre bilder og tekst -->
 
-	<!--Endre bilder og tekst -->
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  Omega 3 tilskudd, multivitamin og mineral tilskudd, Kreatin Monohydrat og protein pulver ( Jeg bruker Vital Greens, Erteprotein) </li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Proteinpulver.</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Omega tre og vitamin D.</li>
+							
+		</ul>
+	</div>
 
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  Jeg anbelaler minimum 3 økter i uken der du går gjennom hele kroppen. </li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Det kommer an på målet ditt.</li>
-		<li><img src="img/tomas.png" alt="Tomas Fjeldberg" width="32" height="32"></a>  Tre.</li>
-					
-	</ul>
+	<div class="question">
+		<p><b>Kunne du fortalt oss din favoritt-middag, som du føler er noe du kunne anbefalt til et treningskosthold?</b></p>
 
+		<!--Endre bilder og tekst -->
 
-	<p><b> Hva er dine fem favoritt-øvelser?</b></p>
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  300 gram Marinerte søtpoteter med olivenolje, ingefær, gurkemeie, havsalt, svart pepper, spisskummen, cayenne og rosmaring. Samt 150g bacon og 200g laks</li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Ovnstekt salma.</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Renskåret kjøtt med salat og potet.</li>
+							
+		</ul>
+	</div>
 
-	<!--Endre bilder og tekst -->
+	<div class="question">
+		<p><b>Langdistanse eller intervalltrening?</b></p>
 
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  Muscle Up, Knebøy, Markløft, Pullups og Benkpress</li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Markløft, knebøy, benkpress, muscle ups, 
-			front lever</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Støt, knebøy, rykk, markløft og benkpress.</li>
-		
-	</ul>
+		<!--Endre bilder og tekst -->
 
+		<ul>
+							
+			<li><img src="img/martin.jpeg" alt="Martin Antonio" width="32" height="32"></a>  Intervalltrening</li>
+			<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Intervalltrening.</li>
+			<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Kombinasjonen er alltid bedre enn, enten eller.</li>
 
-	<p><b> Hva slags trening uttenfor treningssenter vil du anbefale?</b></p>
-
-	<!--Endre bilder og tekst -->
-
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  Det viktigste er at du får i deg nok mat og har et godt grunnlag. 
-		En gåtur i skogen er fin trening. Du kan også trene på 40, 60 og 100 meter sprint. Det er også mulig å trene i barnehageparker og andre arenaer kun ved bruk av egen kroppsvekt. </li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Calisthenics (kroppsvektstrening).</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Ski, svømming, fjellturer, 
-			ballidretter hva som helst egentlig. </li>
-
-	</ul>
-
-
-	<p><b>Hvis du skal anbefale kosttilskudd, hva vil det være?</b></p>
-
-	<!--Endre bilder og tekst -->
-
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  Omega 3 tilskudd, multivitamin og mineral tilskudd, Kreatin Monohydrat og protein pulver ( Jeg bruker Vital Greens, Erteprotein) </li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Proteinpulver.</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Omega tre og vitamin D.</li>
-						
-	</ul>
-
-
-	<p><b>Kunne du fortalt oss din favoritt-middag, som du føler er noe du kunne anbefalt til et treningskosthold?</b></p>
-
-	<!--Endre bilder og tekst -->
-
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  300 gram Marinerte søtpoteter med olivenolje, ingefær, gurkemeie, havsalt, svart pepper, spisskummen, cayenne og rosmaring. Samt 150g bacon og 200g laks</li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Ovnstekt salma.</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Renskåret kjøtt med salat og potet.</li>
-						
-	</ul>
-
-
-	<p><b>Langdistanse eller intervalltrening?</b></p>
-
-	<!--Endre bilder og tekst -->
-
-	<ul>
-						
-		<li><img src="img/martin.png" alt="Martin Antonio" width="32" height="32"></a>  Intervalltrening</li>
-		<li><img src="img/adrian.jpg" alt="Adrian Paul" width="32" height="32"></a>  Intervalltrening.</li>
-		<li><img src="img/tomas.jpg" alt="Tomas Fjeldberg" width="32" height="32"></a>  Kombinasjonen er alltid bedre enn, enten eller.</li>
-
-	</ul>
+		</ul>
+	</div>
 </div>
