2d02e6339ef5678bf092312cb5a5a61d044918fe
amundring <amund@dhcp-158-39-197-124.wlan.hioa.no>
2013-11-14 16:18:43 +0100

    Om oss

--- Websiden/pages/omoss.php
+++ Websiden/pages/omoss.php
@@ -1,6 +1,6 @@
 <div class ="omoss">	
 	<h2>Om oss</h2>
-	<p id="omosstekst">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseruntmollit anim id est laborum:</p>
+	<p id="omosstekst">Denne websiden ble laget i sammenheng med et prosjekt i faget Webprosjekt ved Høgskolen i Oslo og Akershus. Siden er laget av fire studenter som går dataingeniør-linjen på høgskolen. Studentgruppen består av:</p>
 
 	<div id="bilderavoss">
 		<figure class="omossfigure">
