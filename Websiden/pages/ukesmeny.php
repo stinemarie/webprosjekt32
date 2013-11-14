0b57045d964fa0b5852a142fa9367efb77e81108
idaings <IdaVikaskagSalvesen@dhcp-158-39-193-244.wlan.hioa.no>
2013-11-14 10:40:17 +0100

    ukesmeny

--- Websiden/pages/ukesmeny.php
+++ Websiden/pages/ukesmeny.php
@@ -5,51 +5,41 @@
 	<p>Frokost:</p>
 		<p>Smoothie - Solskinn</p>
 	<p>Lunch:</p>
-		<p></p>
+		<p>Yoghurt med hjemmelaget granola</p>
 	<p>Middag:</p>
 		<p>Squashsalat med kylling, perlespelt og basilikumspesto</p>
-	<p>Nattsnack:</p>
-		<p></p>
 
 <h4>TIRSDAG</h4>
 	<p>Frokost:</p>
 		<p>Smoothie - Lyserød Drøm</p>
 	<p>Lunch:</p>
-		<p></p>
+		<p>Polarbrød med pålett</p>
 	<p>Middag:</p>
 		<p>Pasta med spicy tomatsaus og pecorino</p>
-	<p>Nattsnack:</p>
-		<p></p>
 
 <h4>ONSDAG</h4>
 	<p>Frokost:</p>
 		<p>Smoothie - Salatsmoothie</p>
 	<p>Lunch:</p>
-		<p></p>
+		<p>Yoghurt med hjemmelaget granola</p>
 	<p>Middag:</p>
 		<p>Potet- og spinatsalat med laks</p>
-	<p>Nattsnack:</p>
-		<p></p>
 
 <h4>TORSDAG</h4>
 	<p>Frokost:</p>
 		<p>Smoothie - Ajopachi</p>
 	<p>Lunch:</p>
-		<p></p>
+		<p>Polarbrød med pålegg</p>
 	<p>Middag:</p>
 		<p>Tortellini med tomater og ruccula</p>
-	<p>Nattsnack:</p>
-		<p></p>
 
 <h4>FREDAG</h4>
 	<p>Frokost:</p>
 		<p>Smoothie - Skippern</p>
 	<p>Lunch:</p>
-		<p></p>
+		<p>Nistepakke</p>
 	<p>Middag:</p>
 		<p>Hjemmelaget Pizza</p>
-	<p>Nattsnack:</p>
-		<p></p>
 
 <h4>LØRDAG</h4>
 	<p>Frokost:</p>
@@ -58,15 +48,11 @@
 		<p>Sommersalat med jordbær, eple og spekeskinke</p>
 	<p>Middag:</p>
 		<p>Sitruskylling med asparges og timian</p>
-	<p>Nattsnack:</p>
-		<p></p>
 
 <h4>SØNDAG</h4>
 	<p>Frokost:</p>
-		<p>Omelett/Stekt egg med bacon og salat</p>
+		<p>Potetomelett med brokkoli og salami</p>
 	<p>Lunch:</p>
-		<p></p>
+		<p>Fougasse fylt med kylling, bacon, salat, rød pesto og aioli</p>
 	<p>Middag:</p>
 		<p>Spaghetti med spinat, soltørket tomat, hvitløkschips og feta</p>
-	<p>Nattsnack:</p>
-		<p></p>
