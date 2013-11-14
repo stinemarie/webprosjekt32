79ef179e25acbd130e0e94c59385cb8da4f09b75
stinemarie <stinemarie86@dhcp-158-39-192-253.wlan.hioa.no>
2013-11-14 11:29:18 +0100

    lagt ti bilder i ukesoppskrifter

--- Websiden/pages/ukesmeny.php
+++ Websiden/pages/ukesmeny.php
@@ -1,58 +1,85 @@
-<h2>Ukesmenyoversikt</h2>
-<p>Her er en oversikt over hvilke rett som hører til hvilke dag:</p>
+<div class="oppskrifter">
+	<h2>Ukesmenyoversikt</h2>
 
-<h4>MANDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Solskinn</p>
-	<p>Lunch:</p>
-		<p>Yoghurt med hjemmelaget granola</p>
-	<p>Middag:</p>
-		<p>Squashsalat med kylling, perlespelt og basilikumspesto</p>
 
-<h4>TIRSDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Lyserød Drøm</p>
-	<p>Lunch:</p>
-		<p>Polarbrød med pålett</p>
-	<p>Middag:</p>
-		<p>Pasta med spicy tomatsaus og pecorino</p>
+	<nav class="oppskrift">
+		<div>
+			<ul>
+				<li>
+					<a class="lenke" href="index.php?page=smoothie#sol">
+						<img class="fargefelt" src="./img/ikon/smoothie2.jpg" alt="oppskrift på smoothie">
+						<div class="overlay">
+							<p class="tekst">smoothier!</p>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a class="lenke" href="index.php?page=yoghurt">
+						<div class="overlay">
+							<p class="tekst">Yoghurt med hjemmelaget granola</p>
+						</div>
+						<img class="fargefelt" src="./img/ikon/yoghurtikon.jpg" alt="oppskrift på yoghurt">	
+					</a>
+				</li>
+				<li>
+					<a class="lenke" href="index.php?page=squashsalat">
+						<div class="overlay">
+							<p class="tekst">Squashsalat med kylling, perlespett og basilikum pesto</p>
+						</div>
+						<img class="fargefelt" src="./img/ikon/squashsalatikon.jpg" alt="oppskrift på Squashsalat">	
+					</a>
+				</li>
+			</ul>
+		</div>
+	</nav>								
 
-<h4>ONSDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Salatsmoothie</p>
-	<p>Lunch:</p>
-		<p>Yoghurt med hjemmelaget granola</p>
-	<p>Middag:</p>
-		<p>Potet- og spinatsalat med laks</p>
 
-<h4>TORSDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Ajopachi</p>
-	<p>Lunch:</p>
-		<p>Polarbrød med pålegg</p>
-	<p>Middag:</p>
-		<p>Tortellini med tomater og ruccula</p>
 
-<h4>FREDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Skippern</p>
-	<p>Lunch:</p>
-		<p>Nistepakke</p>
-	<p>Middag:</p>
-		<p>Hjemmelaget Pizza</p>
+	<h4>TIRSDAG</h4>
+		<p>Frokost:</p>
+			<p>Smoothie - Lyserød Drøm</p>
+		<p>Lunch:</p>
+			<p>Polarbrød med pålett</p>
+		<p>Middag:</p>
+			<p>Pasta med spicy tomatsaus og pecorino</p>
 
-<h4>LØRDAG</h4>
-	<p>Frokost:</p>
-		<p>Smoothie - Dark Blue Heaven</p>
-	<p>Lunch:</p>
-		<p>Sommersalat med jordbær, eple og spekeskinke</p>
-	<p>Middag:</p>
-		<p>Sitruskylling med asparges og timian</p>
+	<h4>ONSDAG</h4>
+		<p>Frokost:</p>
+			<p>Smoothie - Salatsmoothie</p>
+		<p>Lunch:</p>
+			<p>Yoghurt med hjemmelaget granola</p>
+		<p>Middag:</p>
+			<p>Potet- og spinatsalat med laks</p>
 
-<h4>SØNDAG</h4>
-	<p>Frokost:</p>
-		<p>Potetomelett med brokkoli og salami</p>
-	<p>Lunch:</p>
-		<p>Fougasse fylt med kylling, bacon, salat, rød pesto og aioli</p>
-	<p>Middag:</p>
-		<p>Spaghetti med spinat, soltørket tomat, hvitløkschips og feta</p>
+	<h4>TORSDAG</h4>
+		<p>Frokost:</p>
+			<p>Smoothie - Ajopachi</p>
+		<p>Lunch:</p>
+			<p>Polarbrød med pålegg</p>
+		<p>Middag:</p>
+			<p>Tortellini med tomater og ruccula</p>
+
+	<h4>FREDAG</h4>
+		<p>Frokost:</p>
+			<p>Smoothie - Skippern</p>
+		<p>Lunch:</p>
+			<p>Nistepakke</p>
+		<p>Middag:</p>
+			<p>Hjemmelaget Pizza</p>
+
+	<h4>LØRDAG</h4>
+		<p>Frokost:</p>
+			<p>Smoothie - Dark Blue Heaven</p>
+		<p>Lunch:</p>
+			<p>Sommersalat med jordbær, eple og spekeskinke</p>
+		<p>Middag:</p>
+			<p>Sitruskylling med asparges og timian</p>
+
+	<h4>SØNDAG</h4>
+		<p>Frokost:</p>
+			<p>Potetomelett med brokkoli og salami</p>
+		<p>Lunch:</p>
+			<p>Fougasse fylt med kylling, bacon, salat, rød pesto og aioli</p>
+		<p>Middag:</p>
+			<p>Spaghetti med spinat, soltørket tomat, hvitløkschips og feta</p>
+</div>
