aef7ab21c78d6bda2e6deab5c1f87f1f981e16d4
idaings <IdaVikaskagSalvesen@dhcp-158-39-193-244.wlan.hioa.no>
2013-11-15 16:06:54 +0100

    Lagt til tabell under styrketrening

--- Websiden/pages/styrketrening.php
+++ Websiden/pages/styrketrening.php
@@ -6,22 +6,50 @@
 <img class="treningsbilde"src="img/treningimg/squats2.jpg" alt="squats2">
 <img class="treningsbilde"src="img/treningimg/squats3.jpg" alt="squats3">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Quadriceps, Calves, Glutes, Hamstrings, Lower Back</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Fin øvelse for lår foran, rumpe, lårets innside, bakside lår og magemuskelaturen.
 Det er viktig med rett rygg i denne øvelsen, for å unngå skader.
-Man står med beina noe spredt, og går sakte ned i knebøy til lårene er parallelt med gulvet. Så går man rolig opp igjen og retter opp kroppen, for å gå tilbake i utgangsposisjonen.
-Dette gjør man da med 10-15knebøy med 3 repetisjoner.
-Her er det viktig med nok vekt, da det skal være slitsomt på hver repetisjon.</p>
+Man står med beina noe spredt, og går sakte ned i knebøy til lårene er parallelt med gulvet. Så går man rolig opp igjen og retter opp kroppen, 
+for å gå tilbake i utgangsposisjonen. Her er det viktig med nok vekt, da det skal være slitsomt på hver repetisjon.</p>
 
-<h4>Utfall</h4>
+
+<h4>Gående utfall</h4>
 <!--
 <img class="treningsbilde"src="img/treningimg/utfall1.jpg" alt="utfall1">
 <img class="treningsbilde"src="img/treningimg/utfall2.jpg" alt="utfall2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Quadriceps, Calves, Glutes, Hamstrings</td>
+		<td>20</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>En annen bra øvelse for bein og rumpe.
 Her kan man velge mellom enten gående utfall og stillestående utfall. Man har en manual med vekt i hver hånd, og ved gående utfall, så 
 setter man den ene foten et godt steg foran den andre også går man ned, slik at man har 90 grader på hvert kne, så er det opp igjen og 
 deretter gjør man det samme med det andre beinet foran det andre. Slik går man frem 10 skritt, og 10 skritt tilbake. 
-Man gjør 10x10 med 3repetisjoner.
+
 Om man velger stillestående, så ville vi anbefalt å ha den bakre foten på en benk. Viktig at man her holder ryggen rett og når man 
 skal senke kroppen, at man går rolig ned helt til det bakerste kneet berører gulvet. Man bruker rumpen og bakside lår for å skyve kroppen 
 opp igjen til startposisjonen.</p>
@@ -31,34 +59,74 @@ opp igjen til startposisjonen.</p>
 <img class="treningsbilde"src="img/treningimg/pushup1.jpg" alt="pushup1">
 <img class="treningsbilde"src="img/treningimg/pushup2.jpg" alt="pushup2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Chest, Shoulders, Triceps</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Denne er bra for Bryst og triceps, hvor man kan bruke kun sin egen kroppsvekt, eller om man ønsker en tyngre øvelse, så kan man legge 
 	vekt på ryggen. Her skal hendene være plassert kun litt bredere enn skulderbredden. Vinkelen skal være ca 45 grader ut ifra kroppen på 
 	armene, og ikke 90 grader. Man går da ned helt til nesetippen/brystet berører gulvet før man presser kroppen opp igjen. Man kan gjøre 
-	øvelsen lettere ved å bytte ut med å stå på tærne til å stå på knærne, eller ved å plassere armene høyere enn føttene. Dette gjør man da 
-	12 ganger, med 3 repetisjoner.</p>
+	øvelsen lettere ved å bytte ut med å stå på tærne til å stå på knærne, eller ved å plassere armene høyere enn føttene.</p>
 
 <h4>Planke med ball</h4>
 <!--
 <img class="treningsbilde"src="img/treningimg/plankeball1.jpg" alt="plankeball1">
 <img class="treningsbilde"src="img/treningimg/plankeball2.jpg" alt="plankeball2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Abdominals</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Her skal man tenke at man skal skyte bekkenet noe frem, og så starter man i første posisjon, som på første bilde, og ruller ballen sakte 
 	frem, slik at man ender i en planke stilling. Her er det viktig at man ruller ballen sakte fremover og bruker magemuskelaturen til å holde 
 	igjen. Når man skal opp så ruller man ballen sakte inn mot seg igjen, slik at man ender opp i startposisjonen, men man skal ikke gå helt 
 	opp, men gjøre seg klar til å gå ned igjen med en gang. På den siste utførelsen på hver repetisjon, så skal man holde plankestilling frem 
-	til man ikke orker mer, før man ruller opp igjen til startposisjonen.
-	Dette gjøres 15ganger med 3repetiosjoner.</p>
+	til man ikke orker mer, før man ruller opp igjen til startposisjonen.</p>
 
 <h4>Situps med Bosu ball</h4>
 <!--
 <img class="treningsbilde"src="img/treningimg/situp1.jpg" alt="situp1">
 <img class="treningsbilde"src="img/treningimg/situp2.jpg" alt="situp2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Abdominals</td>
+		<td>15</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Man plasserer overkroppen på ballen med støtte for hele ryggen, fra skuldrene til hoften. Så skal man ha en grei vinkel på beina og 
 	føttene godt plassert på gulvet. Så skal man puste ut og dra overkroppen opp mot knærne, men stopper når du har en rett rygg. Når du 
 	går tilbake, så er det viktig at man bruker magemuskelaturen for å holde igjen. Man skal da kjenne at ryggen rulles opp med ballen og 
-	ikke bare løftes helt opp, slik at man får brukt hele muskelaturen. Dette gjør man 15 ganger med 3repetisjoner. Her man kan gjøre øvelsen 
-	tyngre ved å plassere vekt i armene, med å holde armene strakt opp over overkroppen.
+	ikke bare løftes helt opp, slik at man får brukt hele muskelaturen. Her man kan gjøre øvelsen tyngre ved å plassere vekt i armene, med
+	 å holde armene strakt opp over overkroppen.
  
 http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 
@@ -67,20 +135,46 @@ http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 <img class="treningsbilde"src="img/treningimg/markloft1.jpg" alt="markloft1">
 <img class="treningsbilde"src="img/treningimg/markloft2.jpg" alt="markloft2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Hamstrings, Calves, Glutes, Lower Back</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Man plasserer beina noe fra hverandre. Så skal man holde vektstangen over føttene (midt mellom hælen og tærne). Det er viktig at man 
 	har tyngdepunktet på hælen og enten står barbeint eller med flate sko. Man bøyer seg ned mot knærne med strake armer og rett rygg. Kan 
 	tenke seg at man skal stikke rumpa ut, mens stangen går strakt ned mot føttene, mens man gjør et gøy på knærne. Viktig at man ikke lar 
 	knærne komme i veien for stangen her. Man presser brystet frem og skuldrene bak. På vei opp, så trekker man inn pusten og strammer 
-	magemusklene for å få en mest mulig stabil rygg.
-	Denne gjøres 12 ganger med 3repetisjoner.</p>
+	magemusklene for å få en mest mulig stabil rygg.</p>
 
 <h4>Sidehev mage - MANGLER BILDE</h4>
 <!--
 <img src="">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	</table>
+</div>
 <p>Man plasserer seg i en sidelangs planke med strake bein. Armen plasseres rett under skulderen, slik at den står stabilt. Man skal da 
-	gjøre et hev og senk med hoftene mens armen og beina står på samme plass. For å få en tyngre øvelse, så kan man ha ekstra vekt på hoften. 
-	Dette gjør man 10ganger på hver side med 3repetisjoner.</p>
+	gjøre et hev og senk med hoftene mens armen og beina står på samme plass. For å få en tyngre øvelse, så kan man ha ekstra vekt på hoften.</p>
 
 
 <h4>Roing i kabelmaskin</h4> 
@@ -88,11 +182,24 @@ http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 <img class="treningsbilde"src="img/treningimg/roing1.jpg" alt="roing1">
 <img class="treningsbilde"src="img/treningimg/roing2.jpg" alt="roing2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Middle Back, Biceps, Lats, Shoulders</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>I startposisjonen så sitter man med en rett rygg og beina plassert på platen med en liten bøy i knærne. Man senker skuldrene ned og 
 	tilbake. Så plasserer man blikket foran seg og trekker håndtaket kontrollert mot brystet. Man kan tenke at man skal holde en blyant 
 	sammen med skulderbladene når man trekker håndtaket mot brystet og skyver armene bakover. Viktig at man ikke hjelper til med nakken her, 
-	men nakken skal være i samme posisjon hele tiden.
-	Dette gjør man 12ganger med 3repetisjoner.</p>
+	men nakken skal være i samme posisjon hele tiden.</p>
 
 
 <h4>Løft med step</h4>
@@ -100,9 +207,22 @@ http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 <img class="treningsbilde"src="img/treningimg/loftstep1.jpg" alt="loftstep1">
 <img class="treningsbilde"src="img/treningimg/loftstep2.jpg" alt="loftstep2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Triceps, Chest, Shoulders</td>
+		<td>10</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Plasser hendene på steppen på hver side av hoften ca. Så tar man beina foran steppen, slik at når du senker rumpa ned mot gulvet at du 
-	får en 90 graders vinkel på knærne. Man senker sakte ned mot gulvet, slik at rumpa så vidt berører før man da hever kroppen igjen.
-	Dette gjør man 15ganger med 3repetisjoner.</p>
+	får en 90 graders vinkel på knærne. Man senker sakte ned mot gulvet, slik at rumpa så vidt berører før man da hever kroppen igjen.</p>
 
 
 <h4>Sving med ball (side mage)</h4>
@@ -111,10 +231,23 @@ http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 <img class="treningsbilde"src="img/treningimg/svingball2.jpg" alt="svingball2">
 <img class="treningsbilde"src="img/treningimg/svingball3.jpg" alt="svingball3">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Abdominals</td>
+		<td>30 (15 hver side)</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Man skal sitte på rumpen med rett rygg. Her kan man velge om man ønsker å heve beina fra gulvet eller ikke (tyngre å løfte de fra gulvet). 
 	Man holder da en vekt/vektball som man svinger godt fra den ene siden av kroppen til den andre, mens man da holder balansen og er noe 
-	bakoverlent. Viktig at man da strammer magemusklene her og svinger godt til siden.
-	Dette gjør man da 30ganger (dvs 15 ganger på hver side) med 3repetisjoner.</p>
+	bakoverlent. Viktig at man da strammer magemusklene her og svinger godt til siden.</p>
 
 
 <h4>Armstrekk med vekter</h4> 
@@ -122,10 +255,23 @@ http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 <img class="treningsbilde"src="img/treningimg/armstrekker1.jpg" alt="armstrekker1">
 <img class="treningsbilde"src="img/treningimg/armstrekker2.jpg" alt="armstrekker2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	</table>
+</div>
 <p>Man plasserer den ene foten på gulvet, mens den andre har man på benken. Armen på samme side som beinet på benken plasseres foran 
 	brystet på banken mens den andre armen holdes rett ned under skulderen. Armen trekkes da opp på siden av brystet med albuen bakover 
-	og opp. Man trekker den helt opp, slik at vekten er parallelt med brystet før man senger den ned igjen.
-	Dette gjør man 12ganger på hver arm med 3repetisjoner.</p>
+	og opp. Man trekker den helt opp, slik at vekten er parallelt med brystet før man senger den ned igjen.</p>
 
 
 <h4>Skulderløft med vekter</h4>
@@ -133,9 +279,22 @@ http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 <img class="treningsbilde"src="img/treningimg/skulderloft1.jpg" alt="Skulderloft1">
 <img class="treningsbilde"src="img/treningimg/skulderloft2.jpg" alt="Skulderloft2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Shoulders</td>
+		<td>10</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Ha armene helt strake med en vekt i hver hånd. Så skal armene heves strakt ut fra kroppen. Så stopper man når de er på en rett 
-	linje med lik høyde som skuldrene, så holder man i noen sekunder, før man senker armene igjen.
-	Dette gjør man 10ganger med 3 repetisjoner.</p>
+	linje med lik høyde som skuldrene, så holder man i noen sekunder, før man senker armene igjen.</p>
 
 
 <h4>Liggende benløft</h4>
@@ -143,15 +302,63 @@ http://www.abcnyheter.no/livet/2012/05/22/stram-deg-opp-foer-sommeren</p>
 <img class="treningsbilde"src="img/treningimg/beinloft1.jpg" alt="beinloft1">
 <img class="treningsbilde"src="img/treningimg/beinloft2.jpg" alt="beinloft2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Abdominals</td>
+		<td>10</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Man ligger rett på gulvet med armene strakt på hver side av kroppen. Så skal man heve og senke beina mens de er strake. Her skal 
 	man da bruke magen til å holde igjen når man senker beina ned mot gulvet. Man skal holde igjen når man nesten berører gulvet, og så 
-	skal man heve de igjen.
-	Dette gjør man 10ganger med 3repetisjoner</p>
+	skal man heve de igjen.</p>
 
-<h4>rygghev</h4>
+<h4>Rygghev</h4>
 <!--
 <img class="treningsbilde"src="img/treningimg/rygghev1.jpg" alt="rygghev1">
 <img class="treningsbilde"src="img/treningimg/rygghev2.jpg" alt="rygghev2">
 -->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Lower Back, Glutes, Hamstrings</td>
+		<td>12</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
 <p>Man ligger med strak rygg, så trekker man magen inn og hever brystet mens man krummer ryggen noe.
-Gjøres 12ganger med 3 repetisjoner.</p>
+</p>
+
+<h4>TRX mage</h4>
+<!--
+<img class="treningsbilde"src="img/treningimg/trxmage1.jpg" alt="trxmage1">
+<img class="treningsbilde"src="img/treningimg/trxmage2.jpg" alt="trxmage2">
+-->
+<div class="treninghvor">
+	<table>
+	<thead>
+		<th>Muskelgruppe</th>
+		<th>Repetisjoner</th>
+		<th>Serier</th>
+	</thead>
+	<tr class="alt">
+		<td>Abdominals</td>
+		<td>15</td>
+		<td>3</td>
+	</tr>
+	</table>
+</div>
+<p></p>
