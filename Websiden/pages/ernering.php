6b5945c2aae634e15ce08e2045e638bc0b8b1dc8
idaings <IdaVikaskagSalvesen@new-host.home>
2013-11-16 00:52:11 +0100

    Skrevet ferdig ernæring fra Maiken

--- Websiden/pages/ernering.php
+++ Websiden/pages/ernering.php
@@ -49,13 +49,85 @@
 		væske.</p>
 
 	<p><b>Hva mener du er det viktigste å få i seg hver dag? Mat/drikke?</b></p>
+	<p>Det viktigste er mat. Start dagen med tran, som de sier i reklamen "Det smaker tran, men det er det som funker!". Nordmenn har ofte
+		mangel på Vitamin D, som vi får fra solen, men pga våre langre vintre er det vanskelig å opprettholde en god Vitamin D-status i kroppen.
+		Dette kan du dermed får gjennom fet fisk, hvor du også får i deg Omega 3. Så ta tran og spis fet fisk - Ja takk begge deler!</p>
+	<p>Det man også må tenke på er å få i seg sine 'fem om dagen'. Jeg vil tro folk er lei av det store fokuset på dette, men det er faktisk
+		en grunn til at vi sier det. En ting er at det metter, samtidig som det gir få kcal.</p>
+	<p>Grønnsaker er billig, og frukt er søtt og godt alternativ til sukkerholdige varer. Et eksempel er at 1 kakestykke, tilsvarer 10 stk 
+		gulrøtter. Hva blir du mest mett av? Anbefalt mengde ila. en dag er 750 gr, men synes du det er vanskelig med så mye, og synes det høres
+		helt uoverkommelig ut, så tenk på det på en helt annen måte...prøv å putt minst NOE grønt på hvert måltid. Når du for eksempel skal 
+		spise en skive med leverpostei, så putt en agurkskive, skal du spise ost, ta tomat eller paprika. Skal du lage lasagne, lag med paprika,
+		løk og sopp i røren. Rett og slett, bland det inn i maten, da ser du det ikke og kan late som det ikke finns.</p>
+	<p>Videre er det viktig å få i seg meieriprodukter, dette er sammen med Vitamin D viktig for å styrke skjellettet. Det er viktig å få frem 
+		at man ikke bør kutte ut meieriprodukter fra kostholdet, med mindre man fått påvist en intoleranse hos legen sin. Det er kun 2% i den 
+		norske befolkningen som har laktoseintoleranse, mens antall folk som tror/mener de har det er adskillig mye høyere. De som faktisk har 
+		intoleranse tåler faktisk visse mengder laktose.</p>
+	<p>Til slutt så må man huske å drikke masse vann hver dag. Kroppen består av ca 60% vann, det vil faktisk si at over halvparten av deg er 
+		vann. Vi kan overleve i flere uker uten mat, men uten vann vil du dø ila. noen døgn. Vann er viktig for å opprettholde alle kroppens 
+		funksjoner.</p>
 
 	<p><b>Fisk eller kjøtt?</b></p>
+	<p>Fisk inneholder mange umettede og flerumettede fettsyrer, det såkalte 'sunne fettet'. Fet fisk inneholder i tillegg Omega 3, som er en 
+		viktig kolesterolsenkende fettsyre. Det inneholder også Vitamin D og Jod, som begge er vanskelig å finne i andre matvarer. Det anbefales
+		2-3 middagsporsjoner med fisk per uke. 6 påleggsporsjoner med fisk, tilsvarer 1 middagsporsjon. Dette bør da kombineres av fet (laks, 
+		gir mye Omega 3 og Vitamin D, men er også mer energitett) og mager fisk (tosk og steinbitt).</p>
+	<p>Fisk gir redusert risiko for slag, hjerte- og karsykdommer og prostatakreft. Det er sannsynlig også redusert risiko for bryst-, lunge-, 
+		magesekk-, og tarmkreft.</p>
+	<p>Men både fisk og kjøtt inneholder gode proteiner.</p>
+	<p>Kjøtt er også en god matvare og en flott kilde til protein, som sagt. Det anbefales at man spiser maks 500 gr rødt kjøtt per uke, da
+		høyere andel enn dette gir økt risiko for ulike typer kreft. Det anbefales at man velger magert kjøtt: vilt og lam, eller kjøtt hvor fettet kan fjernes: kylling, kalkun, svin. Kjøtt inneholder 
+	generelt mye mettet fett, som er det 'usunne' fettet. Dette er ikke ønskelig. I malte kjøttproduktee kan man ikke få fjernet fettet 
+	fra matvarene, derfor er disse de mest fettholdige og derfor også 'verstingene' - pølser, medisterdeig, kjøttdeig og Salami. Skal man 
+	spise disse bør man ha fokus på nøkkelmerkede varer, hvor prosentandelen fett er redusert!</p>
+	<p>Så kjøtt er bra det, men man må velge med omhu, at man kan kose seg med deilige kjøttstykker, men velge magrere varianter oftere.</p>
 
 	<p><b>Vegetar eller ikke?</b></p>
+	<p>Vegitarianere har ofte store helsefordeler, men det avhenger i stor grad hva slags vegetarianer man er. På generell basis har 
+		vegetarianere mindre risiko for overvekt, diabetes type 2, kreft osv. De får også i mer næring og mindre salt, mettet fett og generelt
+		ofte mindre kcal. Men det er svært viktig for en vegetarianer å sette seg ordentlig inn i sitt kosthold, for å følge med på at de får
+		i seg de næringstoffene man trenger, da man ofte kan få mangler ved å kutte ut visse matvaregrupper. Vegetarianere er i risikogruppen 
+		for bla. jernmangel og benskjørhet.</p>
+	<p>Ulike typer vegetarianere</p>
+	<ul>
+		<li>Semivegetarianer: Spiser ikke rødt kjøtt</li>
+		<li>Lato-ovo-vegetarianer: Spiser ikke noe kjøtt i det hele tatt</li>
+		<li>Lakto-vegetarianer: Spiser ikke kjøtt eller egg</li>
+		<li>Ovo-vegetarianer: Spiser ikke kjøtt eller meieriprodukter</li>
+		<li>Pecsetarisk vegetarianer: Spiser ikke kjøtt, meieri eller egg, men spiser fisk</li>
+		<li>Veganer: Spiser ikke noe som helst av animalske produkter</li>
+		<li>Rawfood: Kun rå mat</li>
+		<li>Fruktianer: Kun frukt, som må ha dødd på naturlig måte</li>
+		<li>Sproutarian diet: Kun frø, nøtter, belgfrukter, korn osv.</li>
+	</ul>
+	<p>Så er det også det mange kaller "puddingvegetarianere", det er folk som bestemmer seg for å være vegetarianere, men ikke bryr seg
+		noe om verdien av kostholdet. Ofte ungdommer, som da spiser junkfood (pizza, godteri, brus osv.).</p>
+	<p>Selv om det er mange fordeler med ulike typer vegetarianske dietter, vil jeg si at det viktigste, om man spiser vegetarisk, er at man
+		sørger for å få i seg de næringsstoffene som finnes i de matvarene man velger å fjerne fra kostholdet sitt.</p>
 
 	<p><b>Typiske feller? à typisk som folk faller for (trener for mye? Dietter?)</b></p>
+	<p>Alkohol inneholder svært mye kcal. En kveld på byen med ca 6 alkoholenheter, potetgull, nøtter og junkfood på vei hjem, gir fort
+		3000 kcal, og dette blir da i tillegg til det andre som man fikk i seg tidligere den dagen. Samtidig har man da oftes lyst på noe
+		junkfood dagen etter. Så nyt alkoholen, men med måte og prøv å unngå utskeielsene som det ofte bringer med seg.</p>
+	<p>Energybar/Nutrilettbar, som jeg skrev om tidligere. La oss være ærlige...sjokolade er sjokolade! Fordelen er at det i disse også
+		er andre næringsstoffer, men likevel. Energybarer etter trening er også helt unødvendig!</p>
+	<p>Diverse dietter, som trenddietter (teinalderdietten, sykehusdietten, blodtypedietten + flere) har ingen vitenskapelig forskning til
+		grunn for sine anbefalinger, i tillegg starter de ofte bøkene sine med 'glem alt du har hørt, for SLIK ER DET EGENTLIG', og det
+		burde lyse en rød varsellampe for de fleste allerede. Ofte påstandene de kommer med, rett og slett bare svada. De påstår også ofte
+		at maten som blir anbefalt i dag kun bygger på konspirasjonsteorier, som det ikke gjør. Det bygger på svært mange år med forskning!</p>
 
 	<p><b>Hva skal man styre unna? Sundt fett/ikke sundt fett?</b></p>
-
+	<p>'Det er jo sunt, så det kan jeg spise MYE av' - vel, det sier jeg selv. Banan er flott, men man behøver ikke spise ti stk. Nøtter 
+		inneholder sunt fett, men er vært kcal-rike. Salat er ikke lenger så sunn hvis den er dekket i dressing. Tørket frukt er flott, men
+		ofte kcal-rike og sukkerholdige.</p>
+	<p>'Kose seg i helgen' - det skal man absolutt få lov til, men hvis man prøver å gå ned i vekt kan dette være en stor felle. La oss si
+		du spiser 500 kcal mindre enn vanlig hver dag hele uken, men bruker lørdagen på å sluke en sjokolade, en café mocha, et kakestykke
+		og en god porsjon pizza/lasagne osv. Da kan plutselig de 2500 kcal du strevde med å "spare inn" hele uken være bortkastet, og noen 
+		ganger tom. mer enn dette.</p>
+	<p>Dette er den vanligste fellen folk gjør, som slanker seg. De synes at de slanker seg, og slanker seg, og nesten ikke spiser, men
+		så "glemmer" de liksom de gangene de skeier ut. Fete takeaway middager kan fort være over 1000 kcal, og en muffins kan være oppimot 
+		600 kcal. Pluss deretter på den resterende maten du spiser ila dagen, pluss kanskje et glass brus, eller to, en neve nøtter. Da går
+		plutselig ukens regnestykke i pluss til tross for ukens strev.</p>
+	<p>Derfor er mitt tips - Spis litt av alt, men tenk på mengdene, så slipper man å føle at man sulter seg i ukedagene, for så å eksplodere 
+		i helgene!</p>
 </div>
