52b91c0cf3e3547f4fbdff2c1a1129cd5603adb2
idaings <IdaVikaskagSalvesen@new-host.home>
2013-11-15 23:36:29 +0100

    prosjekt, lagt til bilde

--- Websiden/pages/treningsprosjekt.php
+++ Websiden/pages/treningsprosjekt.php
@@ -1,6 +1,6 @@
 <h2>Før/Etter med TTFS</h2>
-<p>Vi har under prosjektperioden gått inn for å ha et Før og Etter prosjekt, som Ida og Christopher har utført. Her satte begge to seg noen
-	små oppnåelige mål for den korte perioden vi hadde, og så skulle vi se hvilke av disse målene som vi følte at vi klarte oppfylle etter
+<p>Vi har under prosjektperioden gått inn for å ha et Før og Etter prosjekt, som Ida har utført. Her satte hun seg noen
+	små oppnåelige mål for den korte perioden vi hadde, og så skulle vi se hvilke av disse målene som hun følte at hun klarte oppfylle etter
 	at prosjektperioden var over.</p>
 <h4>Ida</h4>
 <p>Jeg følte at for meg var det ikke å oppnå å gå ned så, så mange kilo på den ene måneden som vi skulle ha dette prosjektet. For meg så 	gikk det mer ut på å få gjennomført de øktene som jeg vet skal til for at jeg skal kunne sitte igjen med en god selvfølelse. 
@@ -8,16 +8,20 @@
 <ul>
 	<li>Få trent minst 3 ganger hver uke</li>
 	<li>I disse øktene skulle jeg ha minimum 2 intervaller og 2 styrkeøkter fordelt</li>
-	<li>Unngå kollektiv</li>
+	<li>Unngå kollektiv transport</li>
 </ul>
-<p>Nå som vi er ferdig med prosjektet, så kan jeg si at jeg har klart å følge alle mine mål, unntatt den ene uken, da jeg var syk, og kun
-	fikk gjort to av tre ønskede økter den uken.</p>
 
+<div class="styrke">
 <div class="trebilder">
 	<img class="treningsbilde"src="img/treningimg/ape1.jpg" alt="akrobat1">
 	<img class="treningsbilde"src="img/treningimg/ape2.jpg" alt="akrobat2">
 	<img class="treningsbilde"src="img/treningimg/ape3.jpg" alt="akrobat3">
 </div>
+</div>
+
+<br>
+<p>Nå som vi er ferdig med prosjektet, så kan jeg si at jeg har klart å følge alle mine mål, unntatt den ene uken, da jeg var syk, og kun
+	fikk gjort to av tre ønskede økter den uken.</p>
 
 <h2>Skjema over utførelse</h2>
 
