40becb6b7cafdc4cce9b871176add38213e59d56
amundring <amund@dhcp-158-39-197-124.wlan.hioa.no>
2013-11-16 18:48:50 +0100

    Lenker (må fylles inn)

--- Websiden/pages/lenker.php
+++ Websiden/pages/lenker.php
@@ -1,4 +1,64 @@
-<div class="lenker>"
-	<h2>Lenker</h2>
+<h2>Lenker</h2>
+
+<div class="lenker" id="lenkerid">
+	<p>Her har vi samlet... lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore et dolore magna aliqua:</p>
+
+	<h4>Kategori</h4>
+	<div class="lenkesamling">
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+	</div>
+	<figure>
+		<img src="./img/lenkerbilde1.jpg" alt="Skjermbilde fra nettsiden">
+		<figcaption>Skjermbilde fra nettside.no</figcaption>
+	</figure>
+
+	<h4>Kategori</h4>
+	<div class="lenkesamling">
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+	</div>
+	<figure>
+		<img src="./img/lenkerbilde1.jpg" alt="Skjermbilde fra nettsiden">
+		<figcaption>Skjermbilde fra nettside.no</figcaption>
+	</figure>
+
+	<h4>Kategori</h4>
+	<div class="lenkesamling">
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+		<a class="lenke" href="www.google.com" target="_blank">Sidenavn</a>
+		<div class="beskrivelse">– Her skriver man en beskrivelse av siden</div>
+	</div>
+	<figure>
+		<img src="./img/lenkerbilde1.jpg" alt="Skjermbilde fra nettsiden">
+		<figcaption>Skjermbilde fra nettside.no</figcaption>
+	</figure>
+
+
+	
+
 
 </div>
