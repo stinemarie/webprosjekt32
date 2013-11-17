37293aa67d0dab948a272b9e43086b09952d19ef
amundring <amund@dhcp-158-39-197-124.wlan.hioa.no>
2013-11-17 12:48:33 +0100

    Litt CSS på Treningsprosjekt

--- Websiden/pages/treningsprosjekt.php
+++ Websiden/pages/treningsprosjekt.php
@@ -1,234 +1,238 @@
-<h2>Før/Etter med TTFS</h2>
-<p>Vi har under prosjektperioden gått inn for å ha et Før og Etter prosjekt, som Ida har utført. Her satte hun seg noen
-	små oppnåelige mål for den korte perioden vi hadde, og så skulle vi se hvilke av disse målene som hun følte at hun klarte oppfylle etter
-	at prosjektperioden var over.</p>
-<h4>Ida</h4>
-<p>Jeg følte at for meg var det ikke å oppnå å gå ned så, så mange kilo på den ene måneden som vi skulle ha dette prosjektet. For meg så 	gikk det mer ut på å få gjennomført de øktene som jeg vet skal til for at jeg skal kunne sitte igjen med en god selvfølelse. 
-	Mine mål ble derfor:</p>
-<ul>
-	<li>Få trent minst 3 ganger hver uke</li>
-	<li>I disse øktene skulle jeg ha minimum 2 intervaller og 2 styrkeøkter fordelt</li>
-	<li>Unngå kollektiv transport</li>
-</ul>
+<h2>Før/etter med TTFS</h2>
 
-<div class="styrke">
-<div class="trebilder">
-	<img class="treningsbilde"src="img/treningimg/ape1.jpg" alt="akrobat1">
-	<img class="treningsbilde"src="img/treningimg/ape2.jpg" alt="akrobat2">
-	<img class="treningsbilde"src="img/treningimg/ape3.jpg" alt="akrobat3">
-</div>
-</div>
+<div class="treningsprosjekt">
+	<p>Vi har under prosjektperioden gått inn for å ha et Før og Etter prosjekt, som Ida har utført. Her satte hun seg noen
+		små oppnåelige mål for den korte perioden vi hadde, og så skulle vi se hvilke av disse målene som hun følte at hun klarte oppfylle etter
+		at prosjektperioden var over.</p>
+	<h4>Ida:</h4>
+	<p>Jeg følte at for meg var det ikke å oppnå å gå ned så, så mange kilo på den ene måneden som vi skulle ha dette prosjektet. For meg så 	gikk det mer ut på å få gjennomført de øktene som jeg vet skal til for at jeg skal kunne sitte igjen med en god selvfølelse. 
+		Mine mål ble derfor:</p>
+	<ul>
+		<li>Få trent minst 3 ganger hver uke</li>
+		<li>I disse øktene skulle jeg ha minimum 2 intervaller og 2 styrkeøkter fordelt</li>
+		<li>Unngå kollektiv transport</li>
+	</ul>
 
-<br>
-<p>Nå som vi er ferdig med prosjektet, så kan jeg si at jeg har klart å følge alle mine mål, unntatt den ene uken, da jeg var syk, og kun
-	fikk gjort to av tre ønskede økter den uken.</p>
+	<div class="styrke">
+	<div class="trebilder">
+		<img class="treningsbilde" src="img/treningimg/ape1.jpg" alt="akrobat1">
+		<img class="treningsbilde" src="img/treningimg/ape2.jpg" alt="akrobat2">
+		<img class="treningsbilde" src="img/treningimg/ape3.jpg" alt="akrobat3">
+	</div>
+	</div>
 
-<h2>Skjema over utførelse</h2>
+	<br>
+	<p>Nå som vi er ferdig med prosjektet, så kan jeg si at jeg har klart å følge alle mine mål, unntatt den ene uken, da jeg var syk, og kun
+		fikk gjort to av tre ønskede økter den uken.</p>
 
-<div class="treninghvor">
-	<table>
-	<thead>
-		<th>Dato</th>
-		<th>Øvelse</th>
-		<th>Kg</th>
-		<th>Rep</th>
-		<th>Serier</th>
-	</thead>
-	<tr class="alt">
-		<td>27.10.13</td>
-		<td>Intervall 15x15</td>
-		<td>X</td>
-		<td>X</td>
-		<td>30 min</td>
-	</tr>
-	<tr class="alt">
-		<td>27.10.13</td>
-		<td>Sideløft (mage)</td>
-		<td>10 kg</td>
-		<td>30</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>27.10.13</td>
-		<td>TRX mage</td>
-		<td>X</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>30.10.13</td>
-		<td>Markløft</td>
-		<td>45 kg</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>30.10.13</td>
-		<td>Roing, kabelmaskin</td>
-		<td>30 kg</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>30.10.13</td>
-		<td>Vektløft (hver arm)</td>
-		<td>8 kg</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>30.10.13</td>
-		<td>Situps bosu ball</td>
-		<td>6 kg</td>
-		<td>15</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>30.10.13</td>
-		<td>Planke medisin ball</td>
-		<td>X</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>30.10.13</td>
-		<td>Sideløft (mage)</td>
-		<td>10 kg</td>
-		<td>30</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>30.10.13</td>
-		<td>TRX mage</td>
-		<td>X</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td><strong>03.11.13</strong></td>
-		<td><strong>Falt</strong></td>
-		<td><strong>bort</strong></td>
-		<td><strong>pga</strong></td>
-		<td><strong>syk</strong></td>
-	</tr>
-	<tr class="alt">
-		<td>06.11.13</td>
-		<td>Intervall 4x4</td>
-		<td>X</td>
-		<td>X</td>
-		<td>40 min</td>
-	</tr>
-	<tr class="alt">
-		<td>06.11.13</td>
-		<td>Utfall, gående</td>
-		<td>8 kg (hver arm)</td>
-		<td>20</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>06.11.13</td>
-		<td>Roing, kabelmaskin</td>
-		<td>35 kg</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>08.11.13</td>
-		<td>Intervall, i bakke</td>
-		<td>X</td>
-		<td>X</td>
-		<td>30min</td>
-	</tr>
-	<tr class="alt">
-		<td>08.11.13</td>
-		<td>Situps</td>
-		<td>5 kg</td>
-		<td>15</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>08.11.13</td>
-		<td>Pushups</td>
-		<td>X</td>
-		<td>15</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>08.11.13</td>
-		<td>Planken</td>
-		<td>X</td>
-		<td>3</td>
-		<td>2 min</td>
-	</tr>
-	<tr class="alt">
-		<td>08.11.13</td>
-		<td>Spensthopp</td>
-		<td>X</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>10.11.13</td>
-		<td>Joggetur (Bergen)</td>
-		<td>X</td>
-		<td>X</td>
-		<td>1 t 20 min</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>TRX mage</td>
-		<td>X</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>Push ups</td>
-		<td>X</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>Roing, kabelmaskin</td>
-		<td>35 kg</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>Knebøy</td>
-		<td>45 kg</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>Markløft</td>
-		<td>45 kg</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>Armstrekk (hver arm)</td>
-		<td>8 kg</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>Rygghev</td>
-		<td>5 kg</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-	<tr class="alt">
-		<td>12.11.13</td>
-		<td>Liggende beinhev</td>
-		<td>X</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-</table>
-</div>
+	<h2>Skjema over utførelse</h2>
+
+	<div class="treninghvor">
+		<table>
+			<thead>
+				<th>Dato</th>
+				<th>Øvelse</th>
+				<th>Kg</th>
+				<th>Rep</th>
+				<th>Serier</th>
+			</thead>
+			<tr class="alt">
+				<td>27.10.13</td>
+				<td>Intervall 15x15</td>
+				<td>X</td>
+				<td>X</td>
+				<td>30 min</td>
+			</tr>
+			<tr class="alt">
+				<td>27.10.13</td>
+				<td>Sideløft (mage)</td>
+				<td>10 kg</td>
+				<td>30</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>27.10.13</td>
+				<td>TRX mage</td>
+				<td>X</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>30.10.13</td>
+				<td>Markløft</td>
+				<td>45 kg</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>30.10.13</td>
+				<td>Roing, kabelmaskin</td>
+				<td>30 kg</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>30.10.13</td>
+				<td>Vektløft (hver arm)</td>
+				<td>8 kg</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>30.10.13</td>
+				<td>Situps bosu ball</td>
+				<td>6 kg</td>
+				<td>15</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>30.10.13</td>
+				<td>Planke medisin ball</td>
+				<td>X</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>30.10.13</td>
+				<td>Sideløft (mage)</td>
+				<td>10 kg</td>
+				<td>30</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>30.10.13</td>
+				<td>TRX mage</td>
+				<td>X</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td><strong>03.11.13</strong></td>
+				<td><strong>Falt</strong></td>
+				<td><strong>bort</strong></td>
+				<td><strong>pga</strong></td>
+				<td><strong>syk</strong></td>
+			</tr>
+			<tr class="alt">
+				<td>06.11.13</td>
+				<td>Intervall 4x4</td>
+				<td>X</td>
+				<td>X</td>
+				<td>40 min</td>
+			</tr>
+			<tr class="alt">
+				<td>06.11.13</td>
+				<td>Utfall, gående</td>
+				<td>8 kg (hver arm)</td>
+				<td>20</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>06.11.13</td>
+				<td>Roing, kabelmaskin</td>
+				<td>35 kg</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>08.11.13</td>
+				<td>Intervall, i bakke</td>
+				<td>X</td>
+				<td>X</td>
+				<td>30min</td>
+			</tr>
+			<tr class="alt">
+				<td>08.11.13</td>
+				<td>Situps</td>
+				<td>5 kg</td>
+				<td>15</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>08.11.13</td>
+				<td>Pushups</td>
+				<td>X</td>
+				<td>15</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>08.11.13</td>
+				<td>Planken</td>
+				<td>X</td>
+				<td>3</td>
+				<td>2 min</td>
+			</tr>
+			<tr class="alt">
+				<td>08.11.13</td>
+				<td>Spensthopp</td>
+				<td>X</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>10.11.13</td>
+				<td>Joggetur (Bergen)</td>
+				<td>X</td>
+				<td>X</td>
+				<td>1 t 20 min</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>TRX mage</td>
+				<td>X</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>Push ups</td>
+				<td>X</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>Roing, kabelmaskin</td>
+				<td>35 kg</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>Knebøy</td>
+				<td>45 kg</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>Markløft</td>
+				<td>45 kg</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>Armstrekk (hver arm)</td>
+				<td>8 kg</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>Rygghev</td>
+				<td>5 kg</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+			<tr class="alt">
+				<td>12.11.13</td>
+				<td>Liggende beinhev</td>
+				<td>X</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+		</table>
+	</div>
+
+</div>	
