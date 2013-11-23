62bfe9850fa8c77a63aea88569f6548dcbfa590f
stinemarie <stinemarie86@dhcp-158-39-199-142.wlan.hioa.no>
2013-11-23 12:20:09 +0100

    ernering

--- Websiden/pages/ernering.php
+++ Websiden/pages/ernering.php
@@ -2,11 +2,11 @@
 	<h2>Ernæring</h2>
 	<p>Vi har fått Maiken Walle, som er ernæringsstudent på siste året til å svare på noen spørsmål knyttet til kosthold og trening.</p>
 	<p>Maiken Walle har studert på Aleris Medisinske Høgskole</p>
-	<div id="erneringbilder">
-		<img src="./img/maikenwalle.png" alt="maiken walle">	
-		<img src="./img/6omdagen.jpeg" alt="6 om dagen humor bilde">
+	<div class="toBilder">
+		<img class="sBilde" src="./img/maikenwalle.png" alt="maiken walle">	
+		<img class="sBilde "src="./img/6omdagen.jpeg" alt="6 om dagen humor bilde">
 	</div>
-	<p>Kilde til bilde "6 om dagen" : <a href="http://www.jessicasky.bloggersdelight.dk">Jessicasky</a></p>
+	<p class="kilde">Kilde til bilde "6 om dagen" : <a href="http://www.jessicasky.bloggersdelight.dk">Jessicasky</a></p>
 
 	<p><b>Hva burde en hver middag innehold for å få nok energi og næring fra måltidet?</b></p>
 	<p>Enhver middag burde legges opp etter tallerkenmoddelen, hvor man har:</p> 
