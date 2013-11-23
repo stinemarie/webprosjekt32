4f688814871f9b64e07e92f36d213a279133ff71
stinemarie <stinemarie86@dhcp-158-39-199-142.wlan.hioa.no>
2013-11-23 11:43:14 +0100

    bilder på styrke i orden og tilbakeknapp

--- Websiden/pages/styrketrening.php
+++ Websiden/pages/styrketrening.php
@@ -3,48 +3,50 @@
 
 	<h4>Squats med vekter</h4>
 
-	<div id="squatbilder">
-		<img class="treningsbilde" src="img/treningimg/squats1.jpg" alt="squats1">
-		<img class="treningsbilde" src="img/treningimg/squats2.jpg" alt="squats2">
-		<img class="treningsbilde" src="img/treningimg/squats3.jpg" alt="squats3">
+	<div class="treBilder">
+		<img class="sBilde" src="img/treningimg/squats1.jpg" alt="squats1">
+		<img class="sBilde" src="img/treningimg/squats2.jpg" alt="squats2">
+		<img class="sBilde" src="img/treningimg/squats3.jpg" alt="squats3">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Quadriceps, Calves, Glutes, Hamstrings, Lower Back</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Quadriceps, Calves, Glutes, Hamstrings, Lower Back</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Det er viktig med rett rygg i denne øvelsen, for å unngå skader. Man står med beina noe spredt, og går sakte ned i knebøy til lårene er parallelt med gulvet. Så går man rolig opp igjen og retter opp kroppen, for å gå tilbake i utgangsposisjonen. Her er det viktig at man har nok, slik at man blir sliten etter hver repetisjon, og når man er ferdig med alle seriene at man ikke tenker at man ville klart en til.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
+
 	<h4>Utfall</h4>
-	<div id="utfallbilder">
-		<img class="treningsbilde" src="img/treningimg/utfall1.jpg" alt="utfall1">
-		<img class="treningsbilde" src="img/treningimg/utfall2.jpg" alt="utfall2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/utfall1.jpg" alt="utfall1">
+		<img class="sBilde" src="img/treningimg/utfall2.jpg" alt="utfall2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Quadriceps, Calves, Glutes, Hamstrings</td>
-		<td>20</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Quadriceps, Calves, Glutes, Hamstrings</td>
+				<td>20</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Her kan man velge mellom enten gående utfall og stillestående utfall. Det er viktig at man holder ryggen rett og kjernemuskelaturen (mage, skuldre, overkropp) stabil, slik at man unngår tyngde på ryggen. 
@@ -52,282 +54,301 @@
 	<p>Stående utfall: Man kan ha vekt i hver hånd, og starter i en rett stilling. Man må passe på at knær og tær peker i samme retning. Så tar man et stort skritt fremover, og bakre kne bøyes ned til en ca 90 graders vinkel, hvor kneet skal være så nær bakken som mulig, uten å være i kontakt med bakken. Det man må passe på, er at knær ikke går over tær, med at du lener deg for langt frem. Deretter skal man sparke ifra, slik at man kommer opp i utgangsposisjonen.</p>
 
 	<h4>Push ups</h4>
-	<div id="pushupbilder">
-		<img class="treningsbilde" src="img/treningimg/pushup1.jpg" alt="pushup1">
-		<img class="treningsbilde" src="img/treningimg/pushup2.jpg" alt="pushup2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/pushup1.jpg" alt="pushup1">
+		<img class="sBilde" src="img/treningimg/pushup2.jpg" alt="pushup2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Chest, Shoulders, Triceps</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Chest, Shoulders, Triceps</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Det finnes mange forskjellige varianter med push ups. Her bruker man som regel kun sin egen kroppsvekt, eller så kan man gjøre det tyngre ved å evt legge vekt på ryggen. Man står på tærne, eller så kan man gå ned på knærne for å få det lettere. Så går man i en planke possisjon, og strammer magen og sete. Bøy albuene ut til siden og senk brystet ned til gulvet, og press deg deretter opp igjen til en rett posisjon.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
+
 	<h4>Planke med ball</h4>
-	<div id="plankebilder">
-		<img class="treningsbilde" src="img/treningimg/plankeball1.jpg" alt="plankeball1">
-		<img class="treningsbilde" src="img/treningimg/plankeball2.jpg" alt="plankeball2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/plankeball1.jpg" alt="plankeball1">
+		<img class="sBilde" src="img/treningimg/plankeball2.jpg" alt="plankeball2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Abdominals</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Abdominals</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Her skal man tenke at man skal skyte bekkenet noe frem, og så starter man i første posisjon, som på første bilde, og ruller ballen sakte frem, slik at man ender i en planke stilling. Her er det viktig at man ruller ballen sakte fremover og bruker magemuskelaturen til å holde igjen. Når man skal opp så ruller man ballen sakte inn mot seg igjen, slik at man ender opp i startposisjonen. Man skal ikke gå helt opp, men rulle ned igjen med en gang. På den siste repetisjonen på hver serie, så skal man holde plankestilling frem til man ikke orker mer, før man ruller opp igjen til startposisjonen.</p>
 
 	<h4>Situps med Bosu ball</h4>
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/situp1.jpg" alt="situp1">
-		<img class="treningsbilde" src="img/treningimg/situp2.jpg" alt="situp2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/situp1.jpg" alt="situp1">
+		<img class="sBilde" src="img/treningimg/situp2.jpg" alt="situp2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Abdominals</td>
-		<td>15</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Abdominals</td>
+				<td>15</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Man plasserer overkroppen på ballen med støtte for hele ryggen, fra skuldrene til hoften. Så skal man ha en grei vinkel på beina og føttene godt plassert på gulvet. Så skal man puste ut og dra overkroppen opp mot knærne, men stopper når du har en rett rygg. Når du går tilbake, så er det viktig at man bruker magemuskelaturen for å holde igjen. Man skal da kjenne at ryggen rulles opp med ballen og ikke bare løftes helt opp, slik at man får brukt hele muskelaturen. Her man kan gjøre øvelsen tyngre ved å plassere vekt i armene, med å holde armene strakt opp over overkroppen.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
+
 	<h4>Markløft</h4>
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/markloft1.jpg" alt="markloft1">
-		<img class="treningsbilde" src="img/treningimg/markloft2.jpg" alt="markloft2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/markloft1.jpg" alt="markloft1">
+		<img class="sBilde" src="img/treningimg/markloft2.jpg" alt="markloft2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Hamstrings, Calves, Glutes, Lower Back</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Hamstrings, Calves, Glutes, Lower Back</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Man skal holde vektstangen med et over- og underhåndsgrep i skulderbreddes avstand. Så står man med føttene litt fra hverandre, strammer magen, og har rett rygg. Man skal holde beina så rett som mulig, uten å låse knærne. Så skal man bevege overkroppen frem og ned mot føttene, mens hoftene og rumpa går tilbake. Stangen skal gli tettest mulig inntil kroppen. Så retter man seg opp til utgangsposisjon. Det er viktig at man passer på å ha rett rygg, stram mage og puster ut på vei opp.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
+
 	<h4>Roing i kabelmaskin</h4> 
 
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/roing1.jpg" alt="roing1">
-		<img class="treningsbilde" src="img/treningimg/roing2.jpg" alt="roing2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/roing1.jpg" alt="roing1">
+		<img class="sBilde" src="img/treningimg/roing2.jpg" alt="roing2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Middle Back, Biceps, Lats, Shoulders</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Middle Back, Biceps, Lats, Shoulders</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>I startposisjonen så sitter man med en rett rygg og beina plassert på platen med en liten bøy i knærne. Man senker skuldrene ned og tilbake. Så plasserer man blikket foran seg og trekker håndtaket kontrollert mot brystet. Man kan tenke at man skal holde en blyant sammen med skulderbladene når man trekker håndtaket mot brystet og skyver armene bakover. Viktig at man ikke hjelper til med nakken her, men nakken skal være i samme posisjon hele tiden.</p>
 
 	<h4>Dips med step</h4>
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/loftstep1.jpg" alt="loftstep1">
-		<img class="treningsbilde" src="img/treningimg/loftstep2.jpg" alt="loftstep2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/loftstep1.jpg" alt="loftstep1">
+		<img class="sBilde" src="img/treningimg/loftstep2.jpg" alt="loftstep2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Triceps, Chest, Shoulders</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Triceps, Chest, Shoulders</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
+
+	<a href="index.php?page=ovelser">tilbake</a>
 
 	<p>Sett deg på kanten av benken og passer armene tett inntil lårene. Detter tar du da beina ut forran steppen. Så senker man seg ned til armene er i en 90 graders vinkel, og sånn at rumpa så vidt beører gulvet før man hever kroppen igjen. Man skal ikke ha noe hvile mellom hver repetiosjon, slik at når man kommer opp, skal man rett ned igjen, og omvendt.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
 
 	<h4>Sving med ball (side mage)</h4>
-	<div class="trebilder">
-		<img class="treningsbilde" src="img/treningimg/svingball1.jpg" alt="svingball1">
-		<img class="treningsbilde" src="img/treningimg/svingball2.jpg" alt="svingball2">
-		<img class="treningsbilde" src="img/treningimg/svingball3.jpg" alt="svingball3">
+	<div class="treBilder">
+		<img class="sBilde" src="img/treningimg/svingball1.jpg" alt="svingball1">
+		<img class="sBilde" src="img/treningimg/svingball2.jpg" alt="svingball2">
+		<img class="sBilde" src="img/treningimg/svingball3.jpg" alt="svingball3">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Abdominals</td>
-		<td>30 (15 hver side)</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Abdominals</td>
+				<td>30 (15 hver side)</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 	
 	<p>Man skal sitte på rumpen med rett rygg. Her kan man velge om man ønsker å heve beina fra gulvet eller ikke (tyngre å løfte de fra gulvet). Man holder da en vekt/vektball som man svinger godt fra den ene siden av kroppen til den andre, mens man da holder balansen og er noe bakoverlent. Viktig at man da strammer magemusklene her og svinger godt til siden.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
 
 	<h4>Armstrekk med vekter</h4> 
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/armstrekker1.jpg" alt="armstrekker1">
-		<img class="treningsbilde" src="img/treningimg/armstrekker2.jpg" alt="armstrekker2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/armstrekker1.jpg" alt="armstrekker1">
+		<img class="sBilde" src="img/treningimg/armstrekker2.jpg" alt="armstrekker2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Shoulders, Biceps, Lats, Middel Back</td>
-		<td>12 (hver arm)</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Shoulders, Biceps, Lats, Middel Back</td>
+				<td>12 (hver arm)</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 	
 	<p>Man plasserer den ene foten på gulvet, mens den andre har man på benken. Armen på samme side som beinet på benken plasseres foran brystet på banken mens den andre armen holdes rett ned under skulderen. Armen trekkes da opp påsiden av brystet med albuen bakover og opp. Man trekker den helt opp, slik at vekten er parallelt med brystet før man senger den ned igjen.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
+
 	<h4>Sidehev skuldre med vekter</h4>
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/skulderloft1.jpg" alt="Skulderloft1">
-		<img class="treningsbilde" src="img/treningimg/skulderloft2.jpg" alt="Skulderloft2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/skulderloft1.jpg" alt="Skulderloft1">
+		<img class="sBilde" src="img/treningimg/skulderloft2.jpg" alt="Skulderloft2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Shoulders</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Shoulders</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Stå med beina i skulderbreddes avstand, og hold manualene i armene foran kroppen. Med en liten knekk i albueleddet løfter du armene opp til skulderhøyde, og senter ned. Det er viktig at man her ikke løfter høyere enn skulderhøyden, da dette stresser skulderleddet unødvendig.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
 
 	<h4>Liggende benløft</h4>
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/beinloft1.jpg" alt="beinloft1">
-		<img class="treningsbilde" src="img/treningimg/beinloft2.jpg" alt="beinloft2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/beinloft1.jpg" alt="beinloft1">
+		<img class="sBilde" src="img/treningimg/beinloft2.jpg" alt="beinloft2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Abdominals</td>
-		<td>10</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Abdominals</td>
+				<td>10</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Man ligger rett på gulvet med armene strakt på hver side av kroppen. Så skal man heve og senke beina mens de er strake. Når man har beina opp i været, skal man til slutt ta et lite løft av rumpa, hvor man bruker kjernemuskelaturen til å gjøre dette. Når man skal senke beina ned mot gulvet igjen, så er det viktig at man bruker magemuskelene til å holde igjen, til man nesten berører gulvet. Så skal man rett opp igjen uten noe hvile mellom repetisjonene.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
+
 	<h4>Rygghev</h4>
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/rygghev1.jpg" alt="rygghev1">
-		<img class="treningsbilde" src="img/treningimg/rygghev2.jpg" alt="rygghev2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/rygghev1.jpg" alt="rygghev1">
+		<img class="sBilde" src="img/treningimg/rygghev2.jpg" alt="rygghev2">
 	</div>
 
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Lower Back, Glutes, Hamstrings</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Lower Back, Glutes, Hamstrings</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>Man ligger med strak rygg på magen, så kniper man i sete og strammer magen. Så løfter man overkroppen opp fra gulvet, mens man krummer ryggen noe. Så senter man seg ned igjen til utgangsposisjon, og fortsetter uten hvile mellom hver repetisjon.</p>
 
+	<a href="index.php?page=ovelser">tilbake</a>
+
 	<h4>TRX mage</h4>
-	<div class="tobilder">
-		<img class="treningsbilde" src="img/treningimg/trxmage1.jpg" alt="trxmage1">
-		<img class="treningsbilde" src="img/treningimg/trxmage2.jpg" alt="trxmage2">
+	<div class="toBilder">
+		<img class="sBilde" src="img/treningimg/trxmage1.jpg" alt="trxmage1">
+		<img class="sBilde" src="img/treningimg/trxmage2.jpg" alt="trxmage2">
 	</div>
 	<div class="treninghvor">
-	<table>
-	<tr id="tablehead">
-		<th>Muskelgruppe</th>
-		<th>Repetisjoner</th>
-		<th>Serier</th>
-	</tr>
-	<tr class="alt">
-		<td>Lower Back, Glutes, Hamstrings</td>
-		<td>12</td>
-		<td>3</td>
-	</tr>
-	</table>
+		<table>
+			<tr id="tablehead">
+				<th>Muskelgruppe</th>
+				<th>Repetisjoner</th>
+				<th>Serier</th>
+			</tr>
+			<tr class="alt">
+				<td>Lower Back, Glutes, Hamstrings</td>
+				<td>12</td>
+				<td>3</td>
+			</tr>
+		</table>
 	</div>
 
 	<p>TRX løkkene skal henge ca 25 cm over gulvet. Man plasserer én fot i hver løkke, og legger føttene samlet. Man gjør deretter kroppen stiv som en spett, slik at man ligger helt rett, som i en plankepossisjon. Så skyter man rumpa i været og trekker begge knærne opp mot brystet - som når man gjør situps. Så strekker man beina ut igjen, slik at man kommer tilbake til utangsposisjonen. Det skal ikke være noe hvile mellom hver repetisjon.</p>
+
+	<a href="index.php?page=ovelser">tilbake</a>
 </div>
--- Websiden/pages/yogaogpilates.php
+++ Websiden/pages/yogaogpilates.php
@@ -6,7 +6,7 @@
 
 <p>Yoga kan hjelpe deg med å håndtere stress i hverdagen. Samtidig som du styrker kroppen blir du smidig. Pusteøvelsene og meditasjon hjelper deg til å håndtere stress i hverdagen. Her er noen lenker som viser hva Yoga kan gjøre for kropp og sinn. </p>
 <figure>
-	<img class="matbilde" src="img/treningimg/yoga.jpg" alt="en yoga klasse">
+	<img class="sBilde" src="img/treningimg/yoga.jpg" alt="en yoga klasse">
 	<figcaption class="kilde"><a href="http://www.localfitness.com.au">www.localfitness.com.au</a></figcaption>
 </figure>
 
@@ -23,7 +23,7 @@ Les mer om yoga <a href="http://yoga.no/"> her http://yoga.no/<a/> -->
 <p>Pilates, oppkalt etter sin skaper Josef Pilates, har blitt populært bandt annet for sin effektivitet. Treningsformen fokuserer på å styrke rygg og mage muskler. Øvelsene og rolige og skånsomme for kroppen. Her strekker man musklene så vel som å trene de. <br> Pilates kan være rolig og gi deg harmoni som hjelper deg gjennom en stressende hverdag.</p>
 
 <figure>
-	<img class="matbilde" src="img/treningimg/pilates_01.jpg" alt="en kvinne øverpilates">
+	<img class="sbilde" src="img/treningimg/pilates_01.jpg" alt="en kvinne øverpilates">
 	<figcaption class="kilde">Kilde: Producciones ISED Foto: Author J. Vílchez</figcaption>
 </figure>
 
